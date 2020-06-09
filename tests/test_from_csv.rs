32ed2003bef131c1938abc9e3ea68dcbca9e6715
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2020-06-09 13:43:22 +0200

    Updated test

--- tests/test_from_csv.rs
+++ tests/test_from_csv.rs
@@ -2,6 +2,21 @@ extern crate ensmallen_graph;
 use ensmallen_graph::graph::Graph;
 
 #[test]
-fn test_check_consistent_lines() {
-    Graph::from_csv("tests/data/edge_file.tsv", "subject", "object", true);
+fn test_graph_from_csv_edge_only() {
+    let graph = Graph::from_csv(
+        "tests/data/edge_file.tsv".to_string(),
+        "subject".to_string(),
+        "object".to_string(),
+        true,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+    );
 }
