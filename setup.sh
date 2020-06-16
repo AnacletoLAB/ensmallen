34c1d89c3ea989308b34912f48f2c5828c159907
zom <tommaso.fontana.96@gmail.com>
2020-06-16 20:06:26 +0200

    added auto-pip install requirements.txt to setup.hs

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+CURRENT_FOLDER=$(dirname "$(python -c "import os,sys; print(os.path.realpath(sys.argv[1]))" $0)")
 
 # gcc, make, ecc
 sudo apt-get update -qyy
@@ -11,6 +12,7 @@ wget https://repo.anaconda.com/archive/Anaconda3-2020.02-Linux-x86_64.sh -O anac
 bash ./anaconda.sh -b
 echo "export PATH=\$PATH:$HOME/anaconda3/bin" >> $HOME/.bashrc
 $HOME/anaconda3/bin/pip install maturin
+$HOME/anaconda3/bin/pip install $CURRENT_FOLDER/requirements.txt
 
 ###########################################################
 # Setup Rust nightly
