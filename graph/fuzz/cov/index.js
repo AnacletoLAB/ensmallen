3bd7ad3988f654197f37d9a96d0a53898f043029
zom <tommaso.fontana.96@gmail.com>
2020-06-20 18:20:42 +0200

    removed coverage result from repo

--- graph/fuzz/cov/index.js
+++
@@ -1,6 +0,0 @@
-var data = {files:[
-{"link":"only_edge_file.f99990327523e773/index.html","title":"only_edge_file","summary_name":"only_edge_file","covered_class":"lineNoCov","covered":"0.0","covered_lines":"0","uncovered_lines":"0","total_lines" : "0"},
-{"link":"only_edge_file.6555f983e7fecf87/index.html","title":"only_edge_file","summary_name":"only_edge_file","covered_class":"linePartCov","covered":"58.0","covered_lines":"379","uncovered_lines":"275","total_lines" : "654"},
-], merged_files:[]};
-var percent_low = 25;var percent_high = 75;
-var header = { "command" : "only_edge_file", "date" : "2020-06-20 18:18:05", "instrumented" : 654, "covered" : 379,};
--- graph/fuzz/cov/only_edge_file.f99990327523e773/index.js
+++
@@ -1,5 +0,0 @@
-var data = {files:[
-]};
-var percent_low = 25;var percent_high = 75;
-var header = { "command" : "only_edge_file", "date" : "2020-06-20 18:15:38", "instrumented" : 0, "covered" : 0,};
-var merged_data = [];
