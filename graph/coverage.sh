fdb43113c790f526efae7c63df953979e6803e63
zom <tommaso.fontana.96@gmail.com>
2020-06-23 11:08:26 +0200

    updated travis

--- graph/coverage.sh
+++ graph/coverage.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-#rm -rfd target
+rm -rfd target
 # Use a subshell so that we don't modify the user environment-variables
-#cargo test --no-run
+cargo test --no-run
 
 for TEST in $(find ./target/debug/deps -executable -type f | grep test) 
 do 
     mkdir -p ./target/debug/cov/$TEST 
-    kcov --exclude-pattern=/.rustup,/.cargo ./target/debug/cov/$(basename $TEST) $TEST ;\
+    kcov --coveralls-id=$TRAVIS_JOB_ID --exclude-pattern=/.rustup,/.cargo ./target/debug/cov/$(basename $TEST) $TEST ;\
 done; 
 
 kcov --merge ./target/debug/cov/total $(find ./target/debug/cov/ -type d -maxdepth 1 -mindepth 1) 
