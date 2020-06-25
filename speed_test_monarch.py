8a086d804073d2132ea0166c41fcac3d9d840c53
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2020-06-25 15:46:22 +0200

    Updated monarch

--- speed_test_monarch.py
+++ speed_test_monarch.py
@@ -4,6 +4,7 @@ from ensmallen_graph import EnsmallenGraph  # pylint: disable=no-name-in-module
 import compress_json
 import json
 import numpy as np
+from pympler import asizeof
 
 
 start = time()
@@ -39,16 +40,23 @@ median_walks_length = np.median([
     len(walk) for walk in walks
 ])
 
+graph_memory_size = asizeof.asizeof(graph)
+walks_memory_size = asizeof.asizeof(walks)
+
 response = {
-    "required_time": delta,
-    "human_time": naturaldelta(delta),
+    "directory": "monarch",
+    "total_required_time": delta,
     "building_graph_required_time": completed_graph,
-    "building_graph_required_human_time": naturaldelta(completed_graph),
     "random_walk_time": total_walk_time,
-    "random_walk_human_time": naturaldelta(total_walk_time),
     "mean_walks_length": mean_walks_length,
     "median_walks_length": median_walks_length,
-    "traps_rate": graph.traps_rate()
+    "traps_rate": graph.traps_rate(),
+    "graph_size": graph_memory_size,
+    "walks_size": walks_memory_size,
+    "mean_outbound_edges": np.mean(graph.outbounds),
+    "median_outbound_edges": np.median(graph.outbounds),
+    "nodes": graph.get_nodes_number(),
+    "edges": graph.get_edges_number()
 }
 
 print(json.dumps(response, indent=4))
