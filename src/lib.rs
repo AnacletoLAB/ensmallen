fd346238d36f30bfde7b5bac0351c641b5975721
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2020-07-01 12:01:00 +0200

    Added documentation and signature

--- src/lib.rs
+++ src/lib.rs
@@ -17,8 +17,37 @@ fn ensmallen_graph(_py: Python, m: &PyModule) -> PyResult<()> {
 }
 
 #[pyclass]
-#[text_signature = "(edge_path, sourc)"]
-///
+#[text_signature = "(sources, destinations, *, nodes_mapping, nodes_reverse_mapping, node_types, node_types_mapping, node_types_reverse_mapping, edge_types, edge_types_mapping, edge_types_reverse_mapping, weights, validate_input_data, force_conversion_to_undirected)"]
+/// Return new EnsmallenGraph.
+/// 
+/// sources: List[int],
+///     The list of source nodes.
+/// destinations: List[int],
+///     The list of destination nodes.
+/// nodes_mapping: Dict[str, int] = None,
+///     The dictionary with mapping between the node name and its node ID.
+/// nodes_reverse_mapping: List[str] = None,
+///     The reverse mapping between numeric node ID and node name.
+/// node_types: List[int] = None,
+///     List of the node types, must be as long as the nodes mapping.
+/// node_types_mapping: Dict[str, int] = None,
+///     Mapping between the node types names and their IDs.
+/// node_types_reverse_mapping: List[str] = None,
+///     Reverse mapping between numeric node Type IDs and their name.
+/// edge_types: List[int] = None,
+///     List of the egde types, must be as long as the egdes mapping.
+/// edge_types_mapping: Dict[str, int] = None,
+///     Mapping between the edge types names and their IDs.
+/// edge_types_reverse_mapping: List[str] = None,
+///     Reverse mapping between numeric egde Type IDs and their name.
+/// weights: List[float] = None,
+///     List of the weight for each edge.
+/// validate_input_data: bool = True,
+///     Wethever to validate or not the input data.
+/// force_conversion_to_undirected: bool = False,
+///     Wethever to force the conversion from directed graph to undirected
+///     when there are bidirectional directed edges in the given graph.
+/// 
 struct EnsmallenGraph {
     graph: Graph,
 }
