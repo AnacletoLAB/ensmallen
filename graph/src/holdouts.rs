e0445932d263a881b4cc74c7c00c62c38d484957
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2020-07-04 11:34:36 +0200

    Added exception for invalid holdouts percentages

--- graph/src/holdouts.rs
+++ graph/src/holdouts.rs
@@ -33,6 +33,13 @@ impl Graph {
         seed:NodeT,
         train_percentage:f64
     )-> Result<(Graph, Graph), String>{
+
+        if train_percentage<=0.0 || train_percentage>1.0{
+            return Err(String::from(
+                "Given train percentage must be strictly between 0 and 1."
+            ))
+        }
+
         let tree:HashSet<(NodeT, NodeT, Option<EdgeTypeT>)> = self.spanning_tree(seed);
         let mut used_edges:HashSet<(NodeT, NodeT, Option<EdgeTypeT>)> = HashSet::new();
         
