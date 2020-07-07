b61383ffaa7af0124ee622b50da7893ef073c424
zom <tommaso.fontana.96@gmail.com>
2020-07-07 18:28:57 +0200

    added a test for linkprediction

---
+++ graph/tests/test_link_predictions.rs
@@ -0,0 +1,40 @@
+extern crate graph;
+use graph::graph::Graph;
+
+#[test]
+fn test_link_predictions() {
+    let edge_path = "tests/data/edge_file.tsv";
+    let node_path = "tests/data/node_file.tsv";
+    let graph = Graph::from_csv(
+        edge_path,
+        "subject",
+        "object",
+        false,
+        Some("edge_label"),
+        Some("biolink:Association"),
+        Some("weight"),
+        Some(1.0),
+        Some(node_path),
+        Some("id"),
+        Some("category"),
+        Some("biolink:NamedThing"),
+        None,
+        None,
+        None,
+        None,
+        None
+    ).unwrap();
+    graph.walk(10, None, None, None, Some(0),  Some(0.5), Some(2.0), Some(3.0), Some(4.0), None).unwrap();
+
+    let (edges, labels) = graph.link_prediction(
+        0,
+        1000,
+        Some(1.0),
+        None,
+        Some(true)
+    ).unwrap();   
+
+    println!("{}", labels.iter().position(|&r| r == 1u8).unwrap());
+    println!("{}", labels.iter().position(|&r| r == 0u8).unwrap());
+ 
+}
