17f9c1357b75de9d1c9bda5ce3db43d7e27e227b
zom <tommaso.fontana.96@gmail.com>
2020-07-08 20:35:34 +0200

    hopefully fixed the travis build

--- bindings/python/pytests/test_load_ppi.py
+++ bindings/python/pytests/test_load_ppi.py
@@ -1,14 +1,13 @@
 from ensmallen_graph import EnsmallenGraph  # pylint: disable=no-name-in-module
 
-
 def test_load_ppi():
     EnsmallenGraph.from_csv(
-        edge_path="graph/tests/data/ppi.tsv",
+        edge_path="./pytests/data/ppi/edges.tsv",
         sources_column="subject",
         destinations_column="object",
         directed=False,
         weights_column="weight",
-        node_path="graph/tests/data/ppi_nodes.tsv",
+        node_path="./pytests/data/ppi/nodes.tsv",
         nodes_column="id",
         node_types_column="molecular_function",
         default_node_type="Missing"
