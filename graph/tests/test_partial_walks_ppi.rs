03f4a663008b52972593ac7b74215ab3f68ddcdd
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2020-07-13 11:53:26 +0200

    Updated ppi test

--- graph/tests/test_partial_walks_ppi.rs
+++ graph/tests/test_partial_walks_ppi.rs
@@ -1,40 +1,39 @@
 extern crate graph;
 use graph::graph::Graph;
-use graph::{WalksParameters, WalkWeights, SingleWalkParameters};
-
+use graph::{SingleWalkParameters, WalkWeights, WalksParameters};
 
 #[test]
 fn test_partial_walks_ppi() {
     let edge_path = "tests/data/ppi.tsv";
     let node_path = "tests/data/ppi_nodes.tsv";
-    for directed in &[true, false] {
-        let graph = Graph::from_csv(
-            edge_path,
-            "subject",
-            "object",
-            *directed,
-            None,
-            None,
-            Some("weight"),
-            Some(1.0),
-            Some(node_path),
-            Some("id"),
-            Some("category"),
-            Some("biolink:NamedThing"),
-            None,
-            None,
-            None,
-            None,
-            None,
-        )
-        .unwrap().components_holdout(0, 10000).unwrap();
-        let walk_parameters = WalksParameters::new(
-            SingleWalkParameters::new(100, WalkWeights::default()).unwrap(),
-            0,
-            graph.get_not_trap_nodes_number(),
-        )
-        .unwrap()
-        .set_dense_nodes_mapping(Some(graph.get_dense_nodes_mapping()));
-        assert!(graph.walk(&walk_parameters).is_ok());
-    }
+    let graph = Graph::from_csv(
+        edge_path,
+        "subject",
+        "object",
+        false,
+        None,
+        None,
+        Some("weight"),
+        Some(1.0),
+        Some(node_path),
+        Some("id"),
+        Some("category"),
+        Some("biolink:NamedThing"),
+        None,
+        None,
+        None,
+        None,
+        None,
+    )
+    .unwrap()
+    .components_holdout(0, 10000)
+    .unwrap();
+    let walk_parameters = WalksParameters::new(
+        SingleWalkParameters::new(100, WalkWeights::default()).unwrap(),
+        0,
+        graph.get_not_trap_nodes_number(),
+    )
+    .unwrap()
+    .set_dense_nodes_mapping(Some(graph.get_dense_nodes_mapping()));
+    assert!(graph.walk(&walk_parameters).is_ok());
 }
