04c85c474fc077fb5da51d577780abcef841d5fb
zom <tommaso.fontana.96@gmail.com>
2020-07-16 15:25:56 +0200

    fixed fuzzing

--- graph/src/walks_parameters.rs
+++ graph/src/walks_parameters.rs
@@ -1,28 +1,30 @@
 use super::graph::*;
 use super::types::*;
 use rayon::prelude::*;
+use arbitrary::Arbitrary;
 use std::collections::HashMap;
 
+#[derive(Arbitrary, Debug)]
 pub struct WalkWeights {
-    pub(crate) return_weight: ParamsT,
-    pub(crate) explore_weight: ParamsT,
-    pub(crate) change_node_type_weight: ParamsT,
-    pub(crate) change_edge_type_weight: ParamsT,
+    pub return_weight: ParamsT,
+    pub explore_weight: ParamsT,
+    pub change_node_type_weight: ParamsT,
+    pub change_edge_type_weight: ParamsT,
 }
 
 pub struct SingleWalkParameters {
-    pub(crate) length: usize,
-    pub(crate) weights: WalkWeights,
+    pub length: usize,
+    pub weights: WalkWeights,
 }
 
 pub struct WalksParameters {
-    pub(crate) single_walk_parameters: SingleWalkParameters,
-    pub(crate) iterations: usize,
-    pub(crate) min_length: usize,
-    pub(crate) verbose: bool,
-    pub(crate) start_node: NodeT,
-    pub(crate) end_node: NodeT,
-    pub(crate) dense_nodes_mapping: Option<HashMap<NodeT, NodeT>>,
+    pub single_walk_parameters: SingleWalkParameters,
+    pub iterations: usize,
+    pub min_length: usize,
+    pub verbose: bool,
+    pub start_node: NodeT,
+    pub end_node: NodeT,
+    pub dense_nodes_mapping: Option<HashMap<NodeT, NodeT>>,
 }
 
 impl Default for WalkWeights {
