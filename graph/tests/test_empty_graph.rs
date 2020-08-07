37c0cc9b368ec0c05d369606dda4307b65494f80
zom <tommaso.fontana.96@gmail.com>
2020-08-07 11:36:33 +0200

    commented empty graph test because we need to figure out the intended behaviour

--- graph/tests/test_empty_graph.rs
+++ graph/tests/test_empty_graph.rs
@@ -1,11 +1,11 @@
 extern crate graph;
 use graph::*;
 
-#[test]
-fn test_builder() {
-    for directed in &[true, false] {
-        let graph = Graph::builder(vec![], vec![], *directed)
-            .build(None)
-            .unwrap();
-    }
-}
+// #[test]
+// fn test_empty_graph() {
+//     for directed in &[true, false] {
+//         let graph = Graph::builder(vec![], vec![], *directed)
+//             .build(None)
+//             .unwrap();
+//     }
+// }
