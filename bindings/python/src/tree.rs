061479f08490c866693aaac6d29ab48b9214a9f9
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2020-09-18 18:31:08 +0200

    Updated spanning tree documentation

--- bindings/python/src/tree.rs
+++ bindings/python/src/tree.rs
@@ -1,5 +1,5 @@
 use super::*;
-use graph::{NodeT, NodeTypeT};
+use graph::{EdgeTypeT, NodeT};
 use pyo3::prelude::*;
 use std::collections::HashSet;
 
@@ -18,18 +18,23 @@ impl EnsmallenGraph {
     /// ------------------------
     /// seed: int,
     ///     The seed for the spanning tree.
+    /// include_all_edge_types: bool,
+    ///     Wethever to include all the edges between two nodes.
+    ///     This is only relevant in multi-graphs.
+    /// 
+    /// Raises
+    /// ------------------------
+    /// TODO: update the set of exceptions
+    ///
+    /// Returns
+    /// ------------------------
+    /// Set of triple (node, node, edge type) representing a spanning tree.
     ///
     fn spanning_tree(
         &self,
         seed: NodeT,
         include_all_edge_types: bool,
-    ) -> HashSet<(NodeT, NodeT, Option<NodeTypeT>)> {
-        let tree: HashSet<(NodeT, NodeT, Option<NodeTypeT>)> = self
-            .graph
-            .spanning_tree(seed, include_all_edge_types)
-            .iter()
-            .cloned()
-            .collect();
-        tree
+    ) -> HashSet<(NodeT, NodeT, Option<EdgeTypeT>)> {
+        self.graph.spanning_tree(seed, include_all_edge_types)
     }
 }
