92f992a9e437c05b11614fe79c59a77f88e8d2c9
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2020-09-20 10:31:44 +0200

    Finished the bindings to write out the graph

--- bindings/python/src/node_file_writer.rs
+++ bindings/python/src/node_file_writer.rs
@@ -1,40 +1,65 @@
 use super::*;
-use graph::{EdgeTypeT, NodeT};
+use graph::NodeFileWriter;
+use pyo3::exceptions;
 use pyo3::prelude::*;
-use std::collections::HashSet;
+use pyo3::types::PyDict;
 
 #[pymethods]
 impl EnsmallenGraph {
-    #[text_signature = "($self, seed)"]
-    /// Returns set of (typed) edges that form a spanning tree.NodeT
-    ///
-    /// The spanning tree is not minimal or maximal.
-    /// The provided seed is not the root of the tree, but is only needed
-    /// to identify a specific spanning tree.
-    /// This spanning tree algorithm can run also on graph with multiple
-    /// components.
+    #[text_signature = "($self, path, *, verbose, separator, header, nodes_column_number, nodes_column, node_types_column_number, nodes_type_column)"]
+    /// Write to disk the nodes (and optionally the metadata) of the graph.
     ///
     /// Parameters
     /// ------------------------
-    /// seed: int,
-    ///     The seed for the spanning tree.
-    /// include_all_edge_types: bool,
-    ///     Wethever to include all the edges between two nodes.
-    ///     This is only relevant in multi-graphs.
-    /// 
+    /// path: str,
+    ///     Path where to save the nodes and their metadata.
+    /// verbose: bool = True,
+    ///     Wether to show a loading bar while writing to file.
+    /// separator: str = "\t",
+    ///     What separator to use while writing out to file.
+    /// header: bool = True,
+    ///     Wether to write out the header of the file.
+    /// nodes_column_number: int = 0,
+    ///     The column number where to write the nodes.
+    /// nodes_column: str = "id",
+    ///     The name of the column of the nodes.
+    /// node_types_column_number: int = 1,
+    ///     The column number where to write the node types.
+    /// nodes_type_column: str = "category",
+    ///     The name of the column of the node types.
+    ///
     /// Raises
     /// ------------------------
     /// TODO: update the set of exceptions
     ///
-    /// Returns
-    /// ------------------------
-    /// Set of triple (node, node, edge type) representing a spanning tree.
-    ///
-    fn write_node_csv(
-        &self,
-        seed: NodeT,
-        include_all_edge_types: bool,
-    ) -> HashSet<(NodeT, NodeT, Option<EdgeTypeT>)> {
-        self.graph.spanning_tree(seed, include_all_edge_types)
+    fn dump_nodes(&self, path: String, py_kwargs: Option<&PyDict>) -> PyResult<()> {
+        let py = pyo3::Python::acquire_gil();
+        let kwargs = normalize_kwargs!(py_kwargs, py.python());
+
+        validate_kwargs(
+            kwargs,
+            [
+                "verbose",
+                "separator",
+                "header",
+                "nodes_column_number",
+                "nodes_column",
+                "node_types_column_number",
+                "nodes_type_column",
+            ]
+            .iter()
+            .map(|x| x.to_string())
+            .collect(),
+        )?;
+
+        let writer = NodeFileWriter::new(path)
+            .set_verbose(extract_value!(kwargs, "verbose", bool))
+            .set_separator(extract_value!(kwargs, "separator", String))
+            .set_header(extract_value!(kwargs, "header", bool))
+            .set_nodes_column_number(extract_value!(kwargs, "nodes_column_number", usize))
+            .set_nodes_column(extract_value!(kwargs, "nodes_column", String))
+            .set_node_types_column_number(extract_value!(kwargs, "node_types_column_number", usize))
+            .set_node_types_column(extract_value!(kwargs, "nodes_type_column", String));
+        pyex!(writer.dump(&self.graph))
     }
 }
