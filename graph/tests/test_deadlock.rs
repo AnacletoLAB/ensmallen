fc01c30ecc25448a0a550855a26170eeaa9add4c
zom <tommaso.fontana.96@gmail.com>
2020-09-21 09:49:08 +0200

    added another regression test

--- graph/tests/test_deadlock.rs
+++ graph/tests/test_deadlock.rs
@@ -59,12 +59,37 @@ fn test_deadlock2() {
 /// instead of unique_edges_tree.len() < negatives_number
 /// therefore it used to return one edge more than the needed
 /// and if the graph had EXACTLY the number of negative edges as the wanted
-/// it deadlocked
 fn test_deadlock3() {
 
     let edges = vec![
         Ok(("node1".to_string(), "node2".to_string(), Some("type1".to_string()), None)),
         Ok(("node2".to_string(), "node2".to_string(), Some("type2".to_string()), None)),
+    ];
+    let nodes: Option<Vec<Result<(String, Option<String>), String>>> = None;
+
+    let g = graph::Graph::new(
+        edges.iter().cloned(),
+        if let Some(dn) = &nodes {
+            Some(dn.iter().cloned())
+        } else {
+            None
+        },
+        true,
+        false,
+        false,
+        false
+    ).unwrap();
+    graph::test_utilities::default_test_suite(&g, false);
+}
+
+#[test]
+/// this test used to deadlock the sample negatives
+fn test_deadlock4() {
+
+    let edges = vec![
+        Ok(("node1".to_string(), "node1".to_string(),None, None)),
+        Ok(("node1".to_string(), "node2".to_string(),None, None)),
+        Ok(("node2".to_string(), "node2".to_string(), None, None)),
     ];
     let nodes: Option<Vec<Result<(String, Option<String>), String>>> = None;
 
