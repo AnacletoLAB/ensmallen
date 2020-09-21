b26f4f1576944976f98c563f809e6da7e49fa046
zom <tommaso.fontana.96@gmail.com>
2020-09-21 13:43:25 +0200

    added new regression test

--- graph/tests/regression.rs
+++ graph/tests/regression.rs
@@ -87,3 +87,18 @@ fn test_regression6() {
     let g = Graph::from_csv(edges_reader, None, true).unwrap();
     let _ = graph::test_utilities::default_test_suite(&g, false);
 }
+
+#[test]
+/// TBD
+fn test_regression7() {
+    let edges_reader = EdgeFileReader::new("tests/data/regression/7.tsv".to_string()).unwrap()
+        .set_separator(Some(",".to_string()))
+        .set_verbose(Some(false))
+        .set_skip_self_loops(Some(true))
+        .set_ignore_duplicates(Some(true))
+        .set_edge_types_column_number(Some(2)).unwrap()
+        .set_weights_column_number(Some(3)).unwrap()
+        .set_header(Some(false));
+    let g = Graph::from_csv(edges_reader, None, true).unwrap();
+    let _ = graph::test_utilities::default_test_suite(&g, false);
+}
