3b853da85ecc5e5af77b7b7774fa187db4a8f8af
zom <tommaso.fontana.96@gmail.com>
2020-09-21 11:08:42 +0200

    added another new regression test

--- graph/tests/test_regression.rs
+++ graph/tests/test_regression.rs
@@ -23,7 +23,7 @@ fn test_deadlock1() {
         false,
         false
     ).unwrap();
-    graph::test_utilities::default_test_suite(&g, false);
+    let _ = graph::test_utilities::default_test_suite(&g, false);
 }
 
 #[test]
@@ -50,7 +50,7 @@ fn test_deadlock2() {
         false,
         false
     ).unwrap();
-    graph::test_utilities::default_test_suite(&g, false);
+    let _ = graph::test_utilities::default_test_suite(&g, false);
 }
 
 #[test]
@@ -79,7 +79,7 @@ fn test_deadlock3() {
         false,
         false
     ).unwrap();
-    graph::test_utilities::default_test_suite(&g, false);
+    let _ = graph::test_utilities::default_test_suite(&g, false);
 }
 
 #[test]
@@ -105,7 +105,7 @@ fn test_deadlock4() {
         false,
         false
     ).unwrap();
-    graph::test_utilities::default_test_suite(&g, false);
+    let _ = graph::test_utilities::default_test_suite(&g, false);
 }
 
 
@@ -115,31 +115,31 @@ fn test_regression5() {
 
     let edges = vec![
         Ok(("18".to_string(), "0".to_string(), None, None)),
-        Ok(("3".to_string(), "0".to_string(), None, None)),
-        Ok(("8".to_string(), "0".to_string(), None, None)),
+        Ok(("3".to_string(),  "0".to_string(), None, None)),
+        Ok(("8".to_string(),  "0".to_string(), None, None)),
         Ok(("14".to_string(), "0".to_string(), None, None)),
         Ok(("19".to_string(), "0".to_string(), None, None)),
         Ok(("24".to_string(), "0".to_string(), None, None)),
-        Ok(("4".to_string(), "0".to_string(), None, None)),
-        Ok(("9".to_string(), "0".to_string(), None, None)),
+        Ok(("4".to_string(),  "0".to_string(), None, None)),
+        Ok(("9".to_string(),  "0".to_string(), None, None)),
         Ok(("15".to_string(), "0".to_string(), None, None)),
         Ok(("20".to_string(), "0".to_string(), None, None)),
         Ok(("25".to_string(), "0".to_string(), None, None)),
-        Ok(("5".to_string(), "0".to_string(), None, None)),
+        Ok(("5".to_string(),  "0".to_string(), None, None)),
         Ok(("10".to_string(), "0".to_string(), None, None)),
-        Ok(("0".to_string(), "0".to_string(), None, None)),
+        Ok(("0".to_string(),  "0".to_string(), None, None)),
         Ok(("21".to_string(), "0".to_string(), None, None)),
         Ok(("26".to_string(), "0".to_string(), None, None)),
-        Ok(("6".to_string(), "0".to_string(), None, None)),
+        Ok(("6".to_string(),  "0".to_string(), None, None)),
         Ok(("11".to_string(), "0".to_string(), None, None)),
         Ok(("16".to_string(), "0".to_string(), None, None)),
-        Ok(("1".to_string(), "0".to_string(), None, None)),
+        Ok(("1".to_string(),  "0".to_string(), None, None)),
         Ok(("22".to_string(), "0".to_string(), None, None)),
         Ok(("27".to_string(), "0".to_string(), None, None)),
-        Ok(("7".to_string(), "0".to_string(), None, None)),
+        Ok(("7".to_string(),  "0".to_string(), None, None)),
         Ok(("12".to_string(), "0".to_string(), None, None)),
         Ok(("17".to_string(), "0".to_string(), None, None)),
-        Ok(("2".to_string(), "0".to_string(), None, None)),
+        Ok(("2".to_string(),  "0".to_string(), None, None)),
         Ok(("23".to_string(), "0".to_string(), None, None)),
         Ok(("28".to_string(), "0".to_string(), None, None)),
         Ok(("13".to_string(), "0".to_string(), None, None))
@@ -158,5 +158,55 @@ fn test_regression5() {
         false,
         false
     ).unwrap();
-    graph::test_utilities::default_test_suite(&g, false);
+    let _ = graph::test_utilities::default_test_suite(&g, false);
+}
+
+#[test]
+/// this test used to deadlock the sample negatives
+fn test_regression6() {
+    let edges = vec![
+        Ok(("18".to_string(), "0".to_string(), None, None)),
+        Ok(("29".to_string(), "0".to_string(), None, None)),
+        Ok(("14".to_string(), "0".to_string(), None, None)),
+        Ok(("19".to_string(), "0".to_string(), None, None)),
+        Ok(("24".to_string(), "0".to_string(), None, None)),
+        Ok(("4".to_string(), "0".to_string(), None, None)),
+        Ok(("15".to_string(), "0".to_string(), None, None)),
+        Ok(("20".to_string(), "0".to_string(), None, None)),
+        Ok(("5".to_string(), "0".to_string(), None, None)),
+        Ok(("10".to_string(), "0".to_string(), None, None)),
+        Ok(("0".to_string(), "0".to_string(), None, None)),
+        Ok(("21".to_string(), "0".to_string(), None, None)),
+        Ok(("26".to_string(), "0".to_string(), None, None)),
+        Ok(("6".to_string(), "0".to_string(), None, None)),
+        Ok(("16".to_string(), "0".to_string(), None, None)),
+        Ok(("1".to_string(), "0".to_string(), None, None)),
+        Ok(("22".to_string(), "0".to_string(), None, None)),
+        Ok(("27".to_string(), "0".to_string(), None, None)),
+        Ok(("8".to_string(), "3".to_string(), None, None)),
+        Ok(("7".to_string(), "0".to_string(), None, None)),
+        Ok(("12".to_string(), "0".to_string(), None, None)),
+        Ok(("17".to_string(), "0".to_string(), None, None)),
+        Ok(("30".to_string(), "11".to_string(), None, None)),
+        Ok(("2".to_string(), "0".to_string(), None, None)),
+        Ok(("23".to_string(), "0".to_string(), None, None)),
+        Ok(("28".to_string(), "0".to_string(), None, None)),
+        Ok(("25".to_string(), "9".to_string(), None, None)),
+        Ok(("13".to_string(), "0".to_string(), None, None))
+    ];
+    let nodes: Option<Vec<Result<(String, Option<String>), String>>> = None;
+
+    let g = graph::Graph::new(
+        edges.iter().cloned(),
+        if let Some(dn) = &nodes {
+            Some(dn.iter().cloned())
+        } else {
+            None
+        },
+        true,
+        false,
+        false,
+        false
+    ).unwrap();
+    let _ = graph::test_utilities::default_test_suite(&g, false);
 }
