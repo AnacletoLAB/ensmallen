bee01c944ed6aca0aaaf176ea2dda3615382b80a
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2020-09-25 19:07:47 +0200

    Fixed error in trees

--- graph/tests/regression.rs
+++ graph/tests/regression.rs
@@ -145,3 +145,17 @@ fn test_regression10() {
     let g = Graph::from_csv(edges_reader, None, true).unwrap();
     let _ = graph::test_utilities::default_test_suite(&g, false);
 }
+
+#[test]
+/// An unknown cause is making the library crash on this file.
+fn test_regression11() {
+    let edges_reader = EdgeFileReader::new("tests/data/regression/11.tsv".to_string()).unwrap()
+        .set_separator(Some(",".to_string()))
+        .set_verbose(Some(false))
+        .set_skip_self_loops(Some(true))
+        .set_sources_column_number(Some(0)).unwrap()
+        .set_destinations_column_number(Some(1)).unwrap()
+        .set_header(Some(false));
+    let g = Graph::from_csv(edges_reader, None, true).unwrap();
+    let _ = graph::test_utilities::default_test_suite(&g, false);
+}
