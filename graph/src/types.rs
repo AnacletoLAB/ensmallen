686ad8dfcc516b2cdf6a44cd9114eea850905c5a
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2020-10-05 14:46:46 +0200

    Fixed preg-replace error

--- graph/src/types.rs
+++ graph/src/types.rs
@@ -25,7 +25,7 @@ pub type Triple = (NodeT, NodeT, Option<EdgeTypeT>);
 /// Quadruple of edge data
 pub type Quadruple = (NodeT, NodeT, Option<EdgeTypeT>, Option<WeightT>);
 /// Quadrule of string edge data
-pub type StringQuadruple = StringQuadruple;
+pub type StringQuadruple = (String, String, Option<String>, Option<WeightT>);
 
 
 /// Trait used for the Vocabulary class.
