0c991584dac147d58467b187a3f34786e59a0421
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2020-10-23 10:35:32 +0200

    Extended bindings

--- bindings/python/src/drop.rs
+++ bindings/python/src/drop.rs
@@ -15,7 +15,31 @@ impl EnsmallenGraph {
     /// -----------------------------
     /// Cloned graph without edge types.
     fn drop_edge_types(&self) -> PyResult<EnsmallenGraph> {
-        Ok(EnsmallenGraph {graph:pyex!(self.graph.drop_edge_types())?})
+        Ok(EnsmallenGraph {
+            graph: pyex!(self.graph.drop_edge_types())?,
+        })
+    }
+
+    #[args(py_kwargs = "**")]
+    #[text_signature = "($self, verbose)"]
+    /// Returns new graph without singleton nodes.
+    ///
+    /// Note that this will change the internal nodes mapping.
+    ///
+    /// Parameters
+    /// -----------------------------
+    /// verbose: bool = True,
+    ///     Wether to show a loading bar.
+    ///
+    /// Returns
+    /// -----------------------------
+    /// Cloned graph without singleton nodes.
+    fn drop_singletons(&self, verbose: Option<bool>) -> PyResult<EnsmallenGraph> {
+        Ok(EnsmallenGraph {
+            graph: pyex!(self
+                .graph
+                .drop_singletons(verbose.or_else(|| Some(true)).unwrap()))?,
+        })
     }
 
     #[args(py_kwargs = "**")]
@@ -31,7 +55,9 @@ impl EnsmallenGraph {
     /// -----------------------------
     /// Cloned graph without weights.
     fn drop_weights(&self) -> PyResult<EnsmallenGraph> {
-        Ok(EnsmallenGraph {graph:pyex!(self.graph.drop_weights())?})
+        Ok(EnsmallenGraph {
+            graph: pyex!(self.graph.drop_weights())?,
+        })
     }
 
     #[args(py_kwargs = "**")]
@@ -47,6 +73,8 @@ impl EnsmallenGraph {
     /// -----------------------------
     /// Cloned graph without node types.
     fn drop_node_types(&self) -> PyResult<EnsmallenGraph> {
-        Ok(EnsmallenGraph {graph:pyex!(self.graph.drop_node_types())?})        
+        Ok(EnsmallenGraph {
+            graph: pyex!(self.graph.drop_node_types())?,
+        })
     }
 }
