f6b06e4d7761c6bfffb3a544f9f5dd78b1c327cb
zom <tommaso.fontana.96@gmail.com>
2020-11-03 10:03:17 +0100

    renamed the c source

--- graph/build.rs
+++ graph/build.rs
@@ -1,11 +1,11 @@
 // Example custom build script.
 fn main() {
     // Tell Cargo that if the given file changes, to rerun this build script.
-    println!("cargo:rerun-if-changed=src/bad_code.c");
+    println!("cargo:rerun-if-changed=src/core.c");
     // Use the `cc` crate to build a C file and statically link it.
     cc::Build::new()
-        .file("src/bad_code.c")
+        .file("src/core.c")
         .flag("-march=native")
         .flag("-O3")
-        .compile("bad_code.a");
+        .compile("core.a");
 }
