5431e2d2f005083762939ab7ddac00d6a894a117
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2020-11-07 15:22:39 +0100

    Updated printouts

--- graph/src/test_utilities.rs
+++ graph/src/test_utilities.rs
@@ -270,7 +270,9 @@ pub fn default_test_suite(graph: &mut Graph, verbose: bool) -> Result<(), String
             graph.connected_holdout(4, 0.8, None, *include_all_edge_types, verbose)?;
         if graph.connected_components_number(false) != train.connected_components_number(false) {
             println!("GRAPH: {}", graph.textual_report()?);
+            println!("{:?}", graph.get_edge_names(graph.directed));
             println!("TRAIN: {}", train.textual_report()?);
+            println!("{:?}", train.get_edge_names(train.directed));
             panic!("The connected train graph DOES NOT have the same components as the original graph!");
         }
 
