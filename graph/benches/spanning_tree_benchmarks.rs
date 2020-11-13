e04231c85ff3174d3f8ea5d454def4b55fe98062
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2020-11-13 21:42:38 +0100

    Fixed code smells

--- graph/benches/spanning_tree_benchmarks.rs
+++ graph/benches/spanning_tree_benchmarks.rs
@@ -2,11 +2,7 @@
 extern crate test;
 use test::{black_box, Bencher};
 extern crate graph;
-use graph::test_utilities::*;
-
-use graph::test_utilities::{first_order_walker, load_ppi};
-use graph::*;
-use rayon::iter::ParallelIterator;
+use graph::test_utilities::load_ppi;
 
 #[bench]
 fn bench_spanning_arborescence(b: &mut Bencher) {
@@ -14,7 +10,7 @@ fn bench_spanning_arborescence(b: &mut Bencher) {
 
     b.iter(|| {
         for _ in 0..10 {
-            black_box(graph.spanning_arborescence());
+            black_box(graph.spanning_arborescence().unwrap());
         }
     });
 }
@@ -23,11 +19,11 @@ fn bench_spanning_arborescence(b: &mut Bencher) {
 fn bench_spanning_arborescence_with_fast_graph(b: &mut Bencher) {
     let mut graph = load_ppi(true, true, true, false, false, false).unwrap();
 
-    graph.enable_fast_walk(true, true, None);
+    graph.enable_fast_walk(true, true, None).unwrap();
 
     b.iter(|| {
         for _ in 0..10 {
-            black_box(graph.spanning_arborescence());
+            black_box(graph.spanning_arborescence().unwrap());
         }
     });
 }
@@ -47,7 +43,7 @@ fn bench_random_spanning_tree(b: &mut Bencher) {
 fn bench_random_spanning_tree_with_fast_graph(b: &mut Bencher) {
     let mut graph = load_ppi(true, true, true, false, false, false).unwrap();
 
-    graph.enable_fast_walk(true, true, None);
+    graph.enable_fast_walk(true, true, None).unwrap();
 
     b.iter(|| {
         for _ in 0..10 {
