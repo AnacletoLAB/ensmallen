145c1608dac1e70d3510b8e3e357697597feb93e
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2020-12-25 09:14:29 +0100

    Properly exported complete string PPI

--- bindings/python/ensmallen_graph/__init__.py
+++ bindings/python/ensmallen_graph/__init__.py
@@ -1,10 +1,11 @@
 from .ensmallen_graph import EnsmallenGraph # pylint: disable=import-error
 from .ensmallen_graph import preprocessing # pylint: disable=import-error
-from .automatic_graphs import StringPPI, KGCOVID19
+from .automatic_graphs import StringPPI, KGCOVID19, CompleteStringPPI
 
 __all__ = [
     "StringPPI",
     "KGCOVID19",
+    "CompleteStringPPI",
     "EnsmallenGraph",
     "preprocessing"
 ]
