06d8356ba73d918d3b78a3e41bf77c448c75db45
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2021-01-06 14:58:03 +0100

    Introduced automatically retrieved Yue et al. graphs

--- bindings/python/src/from_csv.rs
+++ bindings/python/src/from_csv.rs
@@ -5,7 +5,7 @@ use graph::{EdgeT, Graph, NodeT};
 impl EnsmallenGraph {
     #[staticmethod]
     #[args(py_kwargs = "**")]
-    #[text_signature = "(edge_path, directed, *, directed_edge_list, sources_column_number, sources_column, destinations_column_number, destinations_column, edge_types_column_number, edge_types_column, default_edge_type, weights_column_number, weights_column, default_weight, skip_self_loops, ignore_duplicated_edges, edge_header, edge_rows_to_skip, edge_separator, node_path, nodes_column_number, nodes_column, node_types_column_number, node_types_column, default_node_type, ignore_duplicated_nodes, node_header, node_rows_to_skip, node_separator, numeric_node_ids, numeric_edge_node_ids, numeric_node_type_ids, numeric_edge_type_ids, edge_file_comment_symbol, node_file_comment_symbol, skip_weights_if_unavailable, skip_edge_types_if_unavailable, skip_node_types_if_unavailable, name, verbose)"]
+    #[text_signature = "(edge_path, directed, *, directed_edge_list, sources_column_number, sources_column, destinations_column_number, destinations_column, edge_types_column_number, edge_types_column, default_edge_type, weights_column_number, weights_column, default_weight, skip_self_loops, ignore_duplicated_edges, edge_header, edge_rows_to_skip, edge_max_rows_number, edge edge_separator, node_path, nodes_column_number, nodes_column, node_types_column_number, node_types_column, default_node_type, ignore_duplicated_nodes, node_header, node_rows_to_skip, node_max_rows_number, node_separator, numeric_node_ids, numeric_edge_node_ids, numeric_node_type_ids, numeric_edge_type_ids, edge_file_comment_symbol, node_file_comment_symbol, skip_weights_if_unavailable, skip_edge_types_if_unavailable, skip_node_types_if_unavailable, name, verbose)"]
     /// Return graph loaded from given edge file and optionally node file.
     ///
     /// Parameters
@@ -65,6 +65,8 @@ impl EnsmallenGraph {
     /// edge_rows_to_skip: int = 0,
     ///     If the edge file has some descriptive text in the first few lines,
     ///     this is the parameter that allows you to skip it.
+    /// edge_max_rows_number: int = None,
+    ///     Number of rows to read.
     /// edge_separator: str = "\t",
     ///     The expected separator for the edge file.
     /// node_path: str = None,
@@ -98,6 +100,8 @@ impl EnsmallenGraph {
     /// node_rows_to_skip: int = 0,
     ///     If the node file has some descriptive text in the first few lines,
     ///     this is the parameter that allows you to skip it.
+    /// node_max_rows_number: int = None,
+    ///     Number of rows to read.
     /// node_separator: str = "\t",
     ///      The expected separator for the node file.
     /// numeric_node_ids: bool = False,
@@ -156,7 +160,7 @@ impl EnsmallenGraph {
 
     #[staticmethod]
     #[args(py_kwargs = "**")]
-    #[text_signature = "(edge_path, directed, *, directed_edge_list, sources_column_number, sources_column, destinations_column_number, destinations_column, edge_types_column_number, edge_types_column, default_edge_type, weights_column_number, weights_column, default_weight, skip_self_loops, ignore_duplicated_edges, edge_header, edge_rows_to_skip, edge_separator, node_path, nodes_column_number, nodes_column, node_types_column_number, node_types_column, default_node_type, ignore_duplicated_nodes, node_header, node_rows_to_skip, node_separator, numeric_node_ids, numeric_edge_node_ids, numeric_node_type_ids, numeric_edge_type_ids, edge_file_comment_symbol, node_file_comment_symbol, skip_weights_if_unavailable, skip_edge_types_if_unavailable, skip_node_types_if_unavailable, name, verbose, )"]
+    #[text_signature = "(edge_path, directed, *, directed_edge_list, sources_column_number, sources_column, destinations_column_number, destinations_column, edge_types_column_number, edge_types_column, default_edge_type, weights_column_number, weights_column, default_weight, skip_self_loops, ignore_duplicated_edges, edge_header, edge_rows_to_skip, edge_max_rows_number, edge_separator, node_path, nodes_column_number, nodes_column, node_types_column_number, node_types_column, default_node_type, ignore_duplicated_nodes, node_header, node_rows_to_skip, node_max_rows_number, node_separator, numeric_node_ids, numeric_edge_node_ids, numeric_node_type_ids, numeric_edge_type_ids, edge_file_comment_symbol, node_file_comment_symbol, skip_weights_if_unavailable, skip_edge_types_if_unavailable, skip_node_types_if_unavailable, name, verbose, )"]
     /// Return graph loaded from given edge file and optionally node file.
     ///
     /// Parameters
@@ -216,6 +220,8 @@ impl EnsmallenGraph {
     /// edge_rows_to_skip: int = 0,
     ///     If the edge file has some descriptive text in the first few lines,
     ///     this is the parameter that allows you to skip it.
+    /// edge_max_rows_number: int = None,
+    ///     Number of rows to read.
     /// edge_separator: str = "\t",
     ///     The expected separator for the edge file.
     /// node_path: str = None,
@@ -249,6 +255,8 @@ impl EnsmallenGraph {
     /// node_rows_to_skip: int = 0,
     ///     If the node file has some descriptive text in the first few lines,
     ///     this is the parameter that allows you to skip it.
+    /// node_max_rows_number: int = None,
+    ///     Number of rows to read.
     /// node_separator: str = "\t",
     ///      The expected separator for the node file.
     /// numeric_node_ids: bool = False,
