5dcd84da3399ac009788bc990b4e738523eb8425
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2021-02-12 11:44:23 +0100

    Exposed method

--- graph_miner/__init__.py
+++ graph_miner/__init__.py
@@ -1,7 +1,8 @@
 """Submodule to automatically generated graph methods for graph retrieval."""
-from .repositories import StringGraphRepository, NetworkRepositoryGraphRepository
+from .repositories import StringGraphRepository, NetworkRepositoryGraphRepository, KGHubRepository
 
 __all__ = [
     "StringGraphRepository",
-    "NetworkRepositoryGraphRepository"
+    "NetworkRepositoryGraphRepository",
+    "KGHubRepository"
 ]
