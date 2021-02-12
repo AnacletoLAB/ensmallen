631f12caaf17c2a11a6b15477f2e76650c028c7b
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2021-02-12 11:43:14 +0100

    Exposed method to retrieve data from graphs from KGHub

--- graph_miner/repositories/__init__.py
+++ graph_miner/repositories/__init__.py
@@ -1,8 +1,10 @@
 """Sub-module with repositories offering graph data."""
 from .string_graph_repository import StringGraphRepository
 from .network_repository_graph_repository import NetworkRepositoryGraphRepository
+from .kg_hub_repository import KGHubRepository
 
 __all__ = [
     "StringGraphRepository",
-    "NetworkRepositoryGraphRepository"
+    "NetworkRepositoryGraphRepository",
+    "KGHubRepository"
 ]
