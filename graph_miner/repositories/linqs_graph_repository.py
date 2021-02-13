517f31283dc66a244936ff6114b253697006d2f0
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2021-02-13 08:42:06 +0100

    Updated file path

--- graph_miner/repositories/linqs_graph_repository.py
+++ graph_miner/repositories/linqs_graph_repository.py
@@ -249,7 +249,7 @@ class LINQSGraphRepository(GraphRepository):
             **{
                 parameter: os.path.join(
                     self.repository_package_name,
-                    self.build_stored_graph_name(graph_name),
+                    self.build_stored_graph_name(graph_name).lower(),
                     value
                 )
                 for parameter, value in self._data[graph_name]["callback_arguments"].items()
