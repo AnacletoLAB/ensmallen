c32ea25af87d0bd3fab6d4c897eade7a359f6ca4
Tommaso Fontana <tommaso.fontana.96@gmail.com>
2021-03-15 18:12:40 +0100

    updated the hash bindings

--- bindings/python/src/hash.rs
+++ bindings/python/src/hash.rs
@@ -1,14 +1,10 @@
 use super::*;
-use std::collections::hash_map::DefaultHasher;
-use std::hash::{Hash, Hasher};
 
 #[pymethods]
 impl EnsmallenGraph {
     #[text_signature = "($self)"]
     /// Returns a 64-bit hash of the graph.
     pub fn hash(&self) -> u64 {
-        let mut hasher = DefaultHasher::new();
-        self.graph.hash(&mut hasher);
-        hasher.finish()
+        self.graph.compute_hash();
     }
 }
