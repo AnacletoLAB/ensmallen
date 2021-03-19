76de4a6856121739b680239d7ca36f344ad3f519
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2021-03-19 15:40:16 +0100

    Integrated new non deterministic test

--- graph/tests/test_regression_29.rs
+++ graph/tests/test_regression_29.rs
@@ -1,6 +1,7 @@
 extern crate graph;
 
-use graph::{EdgeFileReader, Graph, NodeFileReader};
+use graph::{EdgeFileReader, Graph};
+use indicatif::{ProgressBar, ProgressIterator, ProgressStyle};
 
 #[test]
 /// This is a regression test that has been automatically generated
@@ -8,7 +9,7 @@ use graph::{EdgeFileReader, Graph, NodeFileReader};
 /// The test originally caused a panic in the file test_utilities.rs,
 /// specifically (at the time) line 379 and column 13.
 ///
-fn test_regression_number_29() -> Result<(), String> {
+fn test_regression_29() -> Result<(), String> {
     let edges_reader = EdgeFileReader::new("tests/data/regression/29.edges")?;
 
     let nodes_reader = None;
@@ -18,8 +19,17 @@ fn test_regression_number_29() -> Result<(), String> {
         nodes_reader,
         false, // Directed
         false, // Directed edge list
-        "Graph",
+        "", // Name of the graph
     )?;
-    let _ = graph::test_utilities::default_test_suite(&mut graph, false);
+
+    let pb = ProgressBar::new(100);
+    pb.set_style(ProgressStyle::default_bar().template(&format!(
+        "{desc} {{spinner:.green}} [{{elapsed_precise}}] [{{bar:40.cyan/blue}}] ({{pos}}/{{len}}, ETA {{eta}})",
+        desc="Executing multiple runs for non-deterministic test"
+    )));
+
+    for _ in (0..100).progress_with(pb){
+        let _ = graph::test_utilities::default_test_suite(&mut graph, false);
+    }
     Ok(())
 }
