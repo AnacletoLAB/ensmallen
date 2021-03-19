edbb5b6936e7675e2319ae11bf3ab1b5898cc28f
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2021-03-19 17:53:32 +0100

    Updated regression tests

---
+++ graph/tests/test_regression_48.rs
@@ -0,0 +1,30 @@
+extern crate graph;
+
+use graph::{Graph, EdgeFileReader};
+
+#[test]
+/// This is a regression test that has been automatically generated
+/// by the fuzzer harness.
+/// The test originally caused a panic in the file test_utilities.rs,
+/// specifically (at the time) line 230 and column 9.
+///
+fn test_regression_48() -> Result<(), String> {
+    let edges_reader = EdgeFileReader::new("tests/data/regression/48.edges")?;
+
+    let nodes_reader = None;
+
+    assert!(
+        Graph::from_unsorted_csv(
+            edges_reader,
+            nodes_reader,
+            false, // Directed
+            true, // Directed edge list
+            "nan" // Name of the graph
+        ).is_err(),
+        concat!(
+            "It should not be possible to load this graph because ",
+            "it is made of an incomplete directed edge list."
+        )
+    );
+    Ok(())
+}
