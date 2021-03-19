6c98c82b164da017764389fe9d93431348b40ccb
Tommaso Fontana <tommaso.fontana.96@gmail.com>
2021-03-19 15:05:11 +0100

    added panic info and graph metadata

--- fuzzing/graph_harness/src/from_csv.rs
+++ fuzzing/graph_harness/src/from_csv.rs
@@ -8,9 +8,11 @@ use std::ffi::OsStr;
 
 #[derive(Arbitrary, Debug, Clone)]
 pub struct FromCsvHarnessParams {
+    directed: bool,
+    directed_edge_list: bool,
     edge_reader: EdgeFileReaderParams,
     nodes_reader: Option<NodeFileReaderParams>,
-    directed: bool,
+    name: String,
 }
 
 #[derive(Arbitrary, Debug, Clone)]
@@ -20,6 +22,7 @@ pub struct CSVFileReaderParams {
     pub header: Option<bool>,
     pub rows_to_skip: Option<usize>,
     pub ignore_duplicates: Option<bool>,
+    pub max_rows_number: Option<u16>,
 }
 
 #[derive(Arbitrary, Debug, Clone)]
@@ -67,7 +70,7 @@ fn get_path(wanted_folder: &str) -> std::path::PathBuf {
     new_path
 }
 
-fn handle_panics(data: FromCsvHarnessParams) {
+fn handle_panics(info: &std::panic::PanicInfo, data: FromCsvHarnessParams) {
     let mut currdir = get_path("ensmallen_graph");
     currdir.push("fuzzing");
     currdir.push("unit_tests");
@@ -75,12 +78,25 @@ fn handle_panics(data: FromCsvHarnessParams) {
     let path = graph::test_utilities::random_path(currdir.to_str());
     std::fs::create_dir_all(&path).unwrap();
 
+    let panic_path = format!("{}/panic.csv", path);
+    let mut file = File::create(panic_path).unwrap();
+    if let Some(s) = info.location() {
+        write!(file, "{},{:?}\n", "file", s.file());
+        write!(file, "{},{:?}\n", "line", s.line());
+        write!(file, "{},{:?}\n", "col", s.column());
+    }
+
+    let edge_metadata_path = format!("{}/graph_metadata.csv", path);
+    let mut file = File::create(edge_metadata_path).unwrap();
+    write!(file, "{},{:?}\n", "directed", data.directed);
+    write!(file, "{},{:?}\n", "directed_edge_list", data.directed_edge_list);
+    write!(file, "{},{:?}\n", "name", data.name);
+
     let edge_path = format!("{}/edges.csv", path);
     std::fs::write(edge_path, data.edge_reader.file);
 
     let edge_metadata_path = format!("{}/edges_metadata.csv", path);
     let mut file = File::create(edge_metadata_path).unwrap();
-    write!(file, "{},{:?}\n", "directed", data.directed);
     write!(file, "{},{:?}\n", "sources_column_number", data.edge_reader.sources_column_number);
     write!(file, "{},{:?}\n", "sources_column", data.edge_reader.sources_column);
     write!(file, "{},{:?}\n", "destinations_column_number", data.edge_reader.destinations_column_number);
@@ -97,6 +113,7 @@ fn handle_panics(data: FromCsvHarnessParams) {
     write!(file, "{},{:?}\n", "header", data.edge_reader.reader.header);
     write!(file, "{},{:?}\n", "rows_to_skip", data.edge_reader.reader.rows_to_skip);
     write!(file, "{},{:?}\n", "ignore_duplicates", data.edge_reader.reader.ignore_duplicates);
+    write!(file, "{},{:?}\n", "max_rows_number", data.edge_reader.reader.max_rows_number);
     
     if let Some(nodes_reader) = data.nodes_reader{
         let node_path = format!("{}/nodes.csv", path);
@@ -114,6 +131,7 @@ fn handle_panics(data: FromCsvHarnessParams) {
         write!(file, "{},{:?}\n", "header", nodes_reader.reader.header);
         write!(file, "{},{:?}\n", "rows_to_skip", nodes_reader.reader.rows_to_skip);
         write!(file, "{},{:?}\n", "ignore_duplicates", nodes_reader.reader.ignore_duplicates);
+        write!(file, "{},{:?}\n", "max_rows_number", nodes_reader.reader.max_rows_number);
     }
 }
 
@@ -122,8 +140,8 @@ pub fn from_csv_harness(data: FromCsvHarnessParams) -> Result<(), String> {
     let nodes_path = graph::test_utilities::random_path(None);
 
     let data_copy = data.clone();
-    std::panic::set_hook(Box::new(move |_| {
-        handle_panics(data_copy.clone());
+    std::panic::set_hook(Box::new(move |info| {
+        handle_panics(info, data_copy.clone());
     }));
 
     internal_harness(&edges_path, &nodes_path, data)
@@ -150,6 +168,7 @@ fn internal_harness(edges_path: &str, nodes_path: &str, data: FromCsvHarnessPara
         .set_edge_types_column(data.edge_reader.edge_types_column)?
         .set_default_edge_type(data.edge_reader.default_edge_type)
         .set_default_weight(data.edge_reader.default_weight)
+        .set_max_rows_number(data.edge_reader.reader.max_rows_number.map(|x| x as u64))
         .set_skip_self_loops(data.edge_reader.skip_self_loops);
 
     let nodes_reader = match data.nodes_reader {
@@ -172,11 +191,12 @@ fn internal_harness(edges_path: &str, nodes_path: &str, data: FromCsvHarnessPara
                     .set_ignore_duplicates(nr.reader.ignore_duplicates)
                     .set_header(nr.reader.header)
                     .set_rows_to_skip(nr.reader.rows_to_skip)
+                    .set_max_rows_number(nr.reader.max_rows_number.map(|x| x as u64))
             )
         }
     };
 
-    let mut g = Graph::from_unsorted_csv(edges_reader, nodes_reader, data.directed, false, "Graph".to_owned())?;
+    let mut g = Graph::from_unsorted_csv(edges_reader, nodes_reader, data.directed, data.directed_edge_list, data.name)?;
     let _ = graph::test_utilities::default_test_suite(&mut g, false);
 
     Ok(())
