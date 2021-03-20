57282b290570fc1bb8250c1959b7588f74991f0b
Tommaso Fontana <tommaso.fontana.96@gmail.com>
2021-03-20 14:00:54 +0100

    added handle panics fromn vec

--- fuzzing/graph_harness/src/handle_panics.rs
+++ fuzzing/graph_harness/src/handle_panics.rs
@@ -47,15 +47,15 @@ pub(crate) fn handle_panics_from_vec(info: &std::panic::PanicInfo, data: FromVec
     std::fs::create_dir_all(&path).unwrap();
     
     // Dump the informations
-    //dump_panic_info(format!("{}/panic.csv", path), info);
-    //dump_graph_metadata(format!("{}/graph_metadata.csv", path), &data);
-    //dump_edges(format!("{}/edges.csv", path), &data.edge_reader.file);
-    //dump_edges_metadata(format!("{}/edges_metadata.csv", path), &data.edge_reader);
+    dump_panic_info(format!("{}/panic.csv", path), info);
+    dump_graph_metadata_from_vec(format!("{}/graph_metadata.csv", path), &data);
+    dump_edges_from_vec(format!("{}/edges.csv", path), &data);
+    dump_edges_metadata_from_vec(format!("{}/edges_metadata.csv", path), &data);
 
     // If there is a node files
-    if let Some(nodes) = data.nodes{
-        //dump_nodes(format!("{}/nodes.csv", path), &nodes_reader.file);
-        //dump_nodes_metadata(format!("{}/nodes_metadata.csv", path), &nodes_reader);
+    if let Some(nodes) = &data.nodes{
+        dump_nodes_from_vec(format!("{}/nodes.csv", path), &nodes);
+        dump_nodes_metadata_from_vec(format!("{}/nodes_metadata.csv", path), &data);
     }
 }
 
@@ -73,6 +73,105 @@ fn get_path(wanted_folder: &str) -> std::path::PathBuf {
     }
 
     new_path
+}
+
+fn dump_graph_metadata_from_vec(path: String, data: &FromVecHarnessParams){
+    let mut file = File::create(path).unwrap();
+    dump!(file, "directed", data.directed);
+    dump!(file, "directed_edge_list", data.directed_edge_list);
+    dump!(file, "name", data.name);
+}
+
+fn dump_nodes_from_vec(path: String, nodes: &Vec<Result<(String, Option<Vec<String>>), String>>){
+    let mut file = File::create(path).unwrap();
+
+    for node_and_type in nodes{
+        if let Ok((node, node_type)) = &node_and_type {
+            write!(
+                file, "{},{:?}\n", 
+                node, node_type.clone().map(|x| x.join("|")),
+            ).expect("Cannot write to file.");
+        }
+    }
+}
+
+fn dump_nodes_metadata_from_vec(path: String, data: &FromVecHarnessParams){
+    let mut file = File::create(path).unwrap();
+    dump!(file, "verbose", false);
+    dump!(file, "separator", ",");
+    dump!(file, "header", false);
+    dump!(file, "rows_to_skip", 0);
+    dump!(file, "ignore_duplicates", false);
+    dump!(file, "max_rows_number", None::<u64>);
+    // nodes specific
+    dump!(file, "default_node_type", None::<u64>);
+    dump!(file, "nodes_column_number", Some(0));
+    dump!(file, "node_types_separator", Some("|"));
+
+    dump!(file, "node_types_column", if data.has_node_types {
+        Some(1)
+    } else {
+        None
+    });
+
+    dump!(file, "node_types_column_number", None::<u64>);
+    dump!(file, "numeric_node_ids", false);
+    dump!(file, "numeric_node_type_ids", false);
+    dump!(file, "skip_node_types_if_unavailable",false);
+    dump!(file, "nodes_column", None::<u64>);
+    dump!(file, "node_types_column", None::<u64>);
+
+}
+
+fn dump_edges_from_vec(path: String, data: &FromVecHarnessParams){
+    let mut file = File::create(path).unwrap();
+    for edge in &data.edges{
+        if let Ok((src, dst, edge_type, weight)) = &edge {
+            write!(
+                file, "{},{},{},{}\n", 
+                src, dst, edge_type.clone().unwrap_or("".to_string()), 
+                weight.map_or("".to_string(), |x| x.to_string())
+            ).expect("Cannot write to file.");
+        }
+    }
+}
+
+fn dump_edges_metadata_from_vec(path: String, data: &FromVecHarnessParams){
+    let mut file = File::create(path).unwrap();
+    dump!(file, "verbose", false);
+    dump!(file, "separator", ",");
+    dump!(file, "header", false);
+    dump!(file, "rows_to_skip", 0);
+    dump!(file, "ignore_duplicates", false);
+    dump!(file, "max_rows_number", None::<u64>);
+    // edge specific
+    dump!(file, "sources_column_number", Some(0));
+    dump!(file, "sources_column", None::<u64>);
+    dump!(file, "destinations_column_number", Some(1));
+    dump!(file, "destinations_column",  None::<u64>);
+    
+    dump!(file, "edge_types_column_number", if data.has_edge_types {
+        Some(2)
+    } else {
+        None
+    });
+
+    dump!(file, "edge_types_column",  None::<u64>);
+
+    dump!(file, "weights_column_number", if data.has_weights {
+        Some(3)
+    } else {
+        None
+    });
+
+    dump!(file, "weights_column",  None::<u64>);
+    dump!(file, "default_weight",  None::<u64>);
+    dump!(file, "default_edge_type",  None::<u64>);
+    dump!(file, "skip_self_loops", false);
+    dump!(file, "numeric_edge_type_ids", false);
+    dump!(file, "numeric_node_ids", false);
+    dump!(file, "skip_weights_if_unavailable", false);
+    dump!(file, "skip_edge_types_if_unavailable", false);
 }
 
 /// Dump the informations about the panic
