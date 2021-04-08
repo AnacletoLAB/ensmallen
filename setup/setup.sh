d6e80fd1d625184b04647f3aa38a1ef8a10addf2
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2021-04-08 16:24:12 +0200

    Restored target

--- setup/setup.sh
+++ setup/setup.sh
@@ -3,7 +3,7 @@ CURRENT_FOLDER=$(dirname "$(python -c "import os,sys; print(os.path.realpath(sys
 
 # gcc, make, ecc
 sudo apt-get update -qyy
-sudo apt-get install build-essential curl wget tmux byobu htop musl-tools -qyy
+sudo apt-get install build-essential curl wget tmux byobu htop musl-tools binutils -qyy
 
 ###########################################################
 # Install anaconda to have an easily reporducible python environments
