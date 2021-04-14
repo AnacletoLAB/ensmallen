4f9579a053e3e7a1230a47c9f59e14bac004908a
Tommaso Fontana <tommaso.fontana.96@gmail.com>
2021-04-14 15:00:04 +0200

    changed the name of the type

--- fuzzing/graph_harness/src/signal_handler.rs
+++ fuzzing/graph_harness/src/signal_handler.rs
@@ -4,7 +4,7 @@ use libc::{signal, sighandler_t};
 use std::sync::Mutex;
 use std::collections::HashMap;
 
-type sig_num = i32;
+type SigNum = i32;
 
 lazy_static! {
     static ref SIG_HANDLER_DATA: Mutex<HashMap<u64, FromVecHarnessParams>> = {
@@ -12,18 +12,18 @@ lazy_static! {
     }; 
 }
 
-pub(crate) fn abrt_handler(sig_num: sig_num) {
-    let mut data_map = SIG_HANDLER_DATA.lock().expect("Cannot get the lock for the sig handlers data");
+pub(crate) fn abrt_handler(sig_num: SigNum) {
+    let data_map = SIG_HANDLER_DATA.lock().expect("Cannot get the lock for the sig handlers data");
     let data = data_map.get(&u64::from(thread::current().id().as_u64())).unwrap();
 
     handle_panics_from_vec(None, data.clone(), Some(sig_num));
 }
 
 
-pub(crate) fn register_handler(sig_num: sig_num, handler: fn(sig_num), data: FromVecHarnessParams) {
+pub(crate) fn register_handler(sig_num: SigNum, handler: fn(SigNum), data: FromVecHarnessParams) {
     unsafe{
         signal(sig_num, 
-            std::mem::transmute::<fn(sig_num), sighandler_t>(handler)
+            std::mem::transmute::<fn(SigNum), sighandler_t>(handler)
         )
     };
 
