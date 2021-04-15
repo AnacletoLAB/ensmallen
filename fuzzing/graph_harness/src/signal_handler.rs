c0c9ab1861e0d3651850c1092c49c46210125557
Tommaso Fontana <tommaso.fontana.96@gmail.com>
2021-04-15 11:01:50 +0200

    re-builded the mega-test

--- fuzzing/graph_harness/src/signal_handler.rs
+++ fuzzing/graph_harness/src/signal_handler.rs
@@ -14,19 +14,23 @@ lazy_static! {
 
 pub(crate) fn abrt_handler(sig_num: SigNum) {
     let data_map = SIG_HANDLER_DATA.lock().expect("Cannot get the lock for the sig handlers data");
-    let data = data_map.get(&u64::from(thread::current().id().as_u64())).unwrap();
 
-    handle_panics_from_vec(None, data.clone(), Some(sig_num));
+    match data_map.get(&u64::from(thread::current().id().as_u64())) {
+        Some(data) => {
+            let _ = handle_panics_from_vec(None, data.clone(), Some(sig_num));
+        },
+        None => {},
+    }
 }
 
 
 pub(crate) fn register_handler(sig_num: SigNum, handler: fn(SigNum), data: FromVecHarnessParams) {
+    let mut data_map = SIG_HANDLER_DATA.lock().expect("Cannot get the lock for the sig handlers data");    
+    data_map.insert(u64::from(thread::current().id().as_u64()), data);
+
     unsafe{
         signal(sig_num, 
             std::mem::transmute::<fn(SigNum), sighandler_t>(handler)
         )
     };
-
-    let mut data_map = SIG_HANDLER_DATA.lock().expect("Cannot get the lock for the sig handlers data");    
-    data_map.insert(u64::from(thread::current().id().as_u64()), data);
 }
