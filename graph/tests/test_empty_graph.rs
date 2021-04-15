9672011a5e2dba01fc73140e46e0fe2d463ce9e8
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2021-04-15 14:06:00 +0200

    Added test for empty graph

---
+++ graph/tests/test_empty_graph.rs
@@ -0,0 +1,12 @@
+extern crate graph;
+
+use graph::test_utilities::*;
+
+#[test]
+fn test_empty_graph() -> Result<(), String> {
+    for directed in &[true, false] {
+        let mut empty_graph = load_empty_graph(*directed);
+        let _ = graph::test_utilities::default_test_suite(&mut empty_graph, false);
+    }
+    Ok(())
+}
