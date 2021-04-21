bfca1f7466da31d44709035af9a042a20213937f
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2021-04-21 21:31:21 +0200

    Added missing bindings

--- bindings/python/src/getters.rs
+++ bindings/python/src/getters.rs
@@ -680,4 +680,40 @@ impl EnsmallenGraph {
     fn get_unknown_node_types_rate(&self) -> PyResult<f64> {
         pe!(self.graph.get_unknown_node_types_rate())
     }
+
+    #[text_signature = "($self)"]
+    /// Returns vector of singleton node IDs of the graph.
+    ///
+    /// [Automatically generated binding]
+    /// [Automatically generated documentation]
+    fn get_singleton_node_ids(&self) -> Vec<NodeT> {
+        self.graph.get_singleton_node_ids()
+    }
+
+    #[text_signature = "($self)"]
+    /// Returns vector of singleton node names of the graph.
+    ///
+    /// [Automatically generated binding]
+    /// [Automatically generated documentation]
+    fn get_singleton_node_names(&self) -> Vec<String> {
+        self.graph.get_singleton_node_names()
+    }
+
+    #[text_signature = "($self)"]
+    /// Returns vector of singleton_with_selfloops node IDs of the graph.
+    ///
+    /// [Automatically generated binding]
+    /// [Automatically generated documentation]
+    fn get_singleton_with_selfloops_node_ids(&self) -> Vec<NodeT> {
+        self.graph.get_singleton_with_selfloops_node_ids()
+    }
+
+    #[text_signature = "($self)"]
+    /// Returns vector of singleton_with_selfloops node names of the graph.
+    ///
+    /// [Automatically generated binding]
+    /// [Automatically generated documentation]
+    fn get_singleton_with_selfloops_node_names(&self) -> Vec<String> {
+        self.graph.get_singleton_with_selfloops_node_names()
+    }
 }
