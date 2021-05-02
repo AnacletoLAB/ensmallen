3b32ce18757747b75f8c4858e89abdf0e64c428b
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2021-05-02 15:29:19 +0200

    Increased number of steps during loading bar

--- graph/src/utils.rs
+++ graph/src/utils.rs
@@ -16,7 +16,9 @@ macro_rules! optionify {
 pub fn get_loading_bar(verbose: bool, desc: &str, total_iterations: usize) -> ProgressBar {
     if verbose {
         let pb = ProgressBar::new(total_iterations as u64);
-        pb.set_draw_delta(total_iterations as u64 / 100);
+        let candidate_iterations = total_iterations as u64 / 1000;
+        let candidate_iterations = candidate_iterations.max(1);
+        pb.set_draw_delta(candidate_iterations);
         pb.set_style(ProgressStyle::default_bar().template(&format!(
             "{desc} {{spinner:.green}} [{{elapsed_precise}}] [{{bar:40.cyan/blue}}] ({{pos}}/{{len}}, ETA {{eta}})",
             desc=desc
