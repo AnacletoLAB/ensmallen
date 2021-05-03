838d7fd33b425986e11b5c744933db73596e08a0
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2021-05-03 21:40:18 +0200

    Fixed parameter

--- bindings/python/src/centrality.rs
+++ bindings/python/src/centrality.rs
@@ -36,13 +36,11 @@ impl EnsmallenGraph {
         )
     }
 
-    #[text_signature = "($self, normalize, verbose)"]
+    #[text_signature = "($self, verbose)"]
     /// Return numpy array with unweighted closeness centrality of the nodes in the considered graph.
     ///
     /// Parameters
     /// ------------------------
-    /// normalize: bool = False,
-    ///     Whether to normalize the values between 0 and 1.
     /// verbose: bool = True,
     ///     Whether to show a loading bar,
     ///
@@ -51,24 +49,23 @@ impl EnsmallenGraph {
     /// Numpy array with unweighted closeness centralities for each node.
     fn get_unweighted_closeness_centrality(
         &self,
-        normalize: Option<bool>,
         verbose: Option<bool>,
     ) -> Py<PyArray1<f64>> {
         let gil = pyo3::Python::acquire_gil();
         to_ndarray_1d!(
             gil,
-            self.graph.get_unweighted_closeness_centrality(normalize, verbose),
+            self.graph.get_unweighted_closeness_centrality(verbose),
             f64
         )
     }
 
-    #[text_signature = "($self, normalize, verbose)"]
+    #[text_signature = "($self, ignore_infinity, verbose)"]
     /// Return numpy array with unweighted harmonic centrality of the nodes in the considered graph.
     ///
     /// Parameters
     /// ------------------------
-    /// normalize: bool = False,
-    ///     Whether to normalize the values between 0 and 1.
+    /// ignore_infinity: bool = False,
+    ///     Whether to ignore infinite distances.
     /// verbose: bool = True,
     ///     Whether to show a loading bar,
     ///
@@ -77,13 +74,13 @@ impl EnsmallenGraph {
     /// Numpy array with unweighted harmonic centralities for each node.
     fn get_unweighted_harmonic_centrality(
         &self,
-        normalize: Option<bool>,
+        ignore_infinity: Option<bool>,
         verbose: Option<bool>,
     ) -> Py<PyArray1<f64>> {
         let gil = pyo3::Python::acquire_gil();
         to_ndarray_1d!(
             gil,
-            self.graph.get_unweighted_harmonic_centrality(normalize, verbose),
+            self.graph.get_unweighted_harmonic_centrality(ignore_infinity, verbose),
             f64
         )
     }
