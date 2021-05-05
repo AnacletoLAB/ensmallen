a7d3f5671c2e09aa8b12ec1790fe1853de965b57
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2021-05-05 17:44:34 +0200

    Resolved code smell

--- graph/src/vertex_cover.rs
+++ graph/src/vertex_cover.rs
@@ -1,6 +1,5 @@
 use super::*;
 use bitvec::prelude::*;
-use indicatif::ParallelProgressIterator;
 use rayon::iter::ParallelIterator;
 use std::collections::HashSet;
 
@@ -10,7 +9,6 @@ impl Graph {
     /// # References
     /// This implementation is described in ["A local-ratio theorem for approximating the weighted vertex cover problem"](http://www.cs.technion.ac.il/~reuven/PDF/vc_lr.pdf).
     pub fn approximated_vertex_cover(&self) -> HashSet<NodeT> {
-        let edges_number = self.get_edges_number() as usize;
         let nodes_number = self.get_nodes_number() as usize;
         let mut covered_nodes = bitvec![Lsb0, u8; 0; nodes_number];
         let thread_shared_covered_nodes = ThreadSafe {
