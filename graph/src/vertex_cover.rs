52e9bace18b51da3db38d95871ba77b752dfb088
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2021-05-05 16:00:28 +0200

    Fixed vertex cover and updated its test

--- graph/src/vertex_cover.rs
+++ graph/src/vertex_cover.rs
@@ -3,17 +3,16 @@ use bitvec::prelude::*;
 use rayon::iter::IntoParallelRefIterator;
 use rayon::iter::ParallelIterator;
 use rayon::iter::{IndexedParallelIterator, ParallelBridge};
-use std::collections::HashSet;
 
 impl Graph {
     /// Returns 2-approximated verted cover using greedy algorithm.
     ///
     /// # Arguments
     /// * `verbose`: Option<bool> - Whether to show a loading bar.
-    pub fn approximated_vertex_cover(&self, verbose: Option<bool>) -> HashSet<NodeT> {
+    pub fn approximated_vertex_cover(&self, verbose: Option<bool>) -> Vec<NodeT> {
         let verbose = verbose.unwrap_or(true);
         let nodes_number = self.get_nodes_number() as usize;
-        let mut vertex_cover = HashSet::new();
+        let mut vertex_cover = Vec::with_capacity(nodes_number);
         let mut covered_nodes = bitvec![Lsb0, u8; 0; nodes_number as usize];
         let mut degrees = self.get_node_degrees();
         let pb = get_loading_bar(
@@ -25,18 +24,22 @@ impl Graph {
             .par_iter()
             .enumerate()
             .filter(|(node_id, _)| !covered_nodes[*node_id])
-            .max_by_key(|&(_, value)| value)
+            .max_by_key(|&(_, degree)| degree)
         {
-            // We do not check for the selfloop or multigrapjs, but since is only for porposes
+            // We do not check for the selfloop or multigraphs, but since is only for porposes
             // of visualization of the process of the algorithm, it does not make sense
             // to bother with additional checks.
             pb.inc(1 + degree as u64);
-            vertex_cover.insert(max_degree_node_id as NodeT);
+            vertex_cover.push(max_degree_node_id as NodeT);
             unsafe {
                 *covered_nodes.get_unchecked_mut(max_degree_node_id) = true;
             }
             if degree == 0 {
-                continue;
+                // If we reached a 0 degree max node, it means that all the
+                // remaining nodes are either singletons or all their neighbours
+                // have been inserted. Therefore, we can now complete sequentially.
+                vertex_cover.extend(covered_nodes.iter_zeros().map(|node_id| node_id as NodeT));
+                break;
             }
             let thread_shared_degrees = ThreadSafe {
                 value: std::cell::UnsafeCell::new(&mut degrees),
