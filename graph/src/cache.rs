8d9193bfa39132cc27196c799fc5380934db1793
TommasoFontana <tommaso.fontana.96@gmail.com>
2021-07-13 11:48:33 +0300

    fixed singleton_nodes_with_selfloops_number

--- graph/src/cache.rs
+++ graph/src/cache.rs
@@ -16,6 +16,7 @@ pub(crate) struct PropertyCache {
     pub(crate) trap_nodes_number: Option<NodeT>,
     pub(crate) selfloops_number: Option<EdgeT>,
     pub(crate) selfloops_number_unique: Option<NodeT>,
+    pub(crate) singleton_nodes_with_selfloops_number: Option<NodeT>,
 }
 
 impl Default for PropertyCache {
@@ -35,6 +36,7 @@ impl Default for PropertyCache {
             trap_nodes_number: None,
             selfloops_number: None,
             selfloops_number_unique: None,
+            singleton_nodes_with_selfloops_number: None,
         }
     }
 }
--- graph/src/getters_cached.rs
+++ graph/src/getters_cached.rs
@@ -2,7 +2,7 @@ use super::*;
 use rayon::iter::ParallelIterator;
 
 impl Graph {
-    /// Compute the maximum and minimum edge weight and cache it
+    /// Compute the maximum, minimum, and total edge weight and cache it
     fn compute_max_and_min_edge_weight(&self) {
         let mut cache = unsafe { &mut (*self.cache.get()) };
 
@@ -134,34 +134,64 @@ impl Graph {
     /// Return the minimum weighted node degree.
     );
 
-    /// Compute how many selfloops and how many **uniques** selfloops the graph contains.
+    /// Compute how many selfloops and how many **uniques** selfloops  and how many singletons with selfloops the graph contains.
     fn compute_selfloops_number(&self) {
-        let (selfloops_number, selfloops_number_unique) = self
+
+        /// Struct with the info we want to collect
+        /// This is just a nice way to handle reduces
+        struct Info {
+            selfloops_number_unique: NodeT,
+            selfloops_number: EdgeT,
+            singleton_nodes_with_selfloops_number: NodeT,
+        }
+
+        impl Default for Info {
+            fn default() -> Self {
+                Info{
+                    selfloops_number_unique: 0,
+                    selfloops_number: 0,
+                    singleton_nodes_with_selfloops_number: 0,
+                }
+            }
+        }
+
+        impl std::ops::Add<Self> for Info {
+            type Output = Self;
+
+            fn add(self, rhs: Self) -> Self::Output {
+                Info{
+                    selfloops_number_unique: self.selfloops_number_unique + rhs.selfloops_number_unique,
+                    selfloops_number: self.selfloops_number + rhs.selfloops_number,
+                    singleton_nodes_with_selfloops_number: self.singleton_nodes_with_selfloops_number + rhs.singleton_nodes_with_selfloops_number,
+                }
+            }
+        }
+
+        let info = self
             .par_iter_node_ids()
             .map(|node_id| {
-                let self_loops_number =
+                let (selfloops_number, degree) =
                     unsafe { self.iter_unchecked_neighbour_node_ids_from_source_node_id(node_id) }
-                        .filter(|x| *x == node_id)
-                        .count();
-                (
-                    self_loops_number as EdgeT,
-                    (if self_loops_number > 0 { 1 } else { 0 }) as NodeT,
-                )
+                        .map(|x| (if x == node_id {1} else {0}, 1))
+                        .reduce(
+                            |(a1, b1), (a2, b2)| (a1 + a2, b1 + b2),
+                        ).unwrap_or((0, 0));
+
+                Info{
+                    selfloops_number: selfloops_number as EdgeT,
+                    selfloops_number_unique: (if selfloops_number > 0 { 1 } else { 0 }) as NodeT,
+                    singleton_nodes_with_selfloops_number: (if selfloops_number == degree { 1 } else { 0 }) as NodeT,
+                }
             })
             .reduce(
-                || (0, 0),
-                |(selfloops_number_a, selfloops_number_unique_a),
-                 (selfloops_number_b, selfloops_number_unique_b)| {
-                    (
-                        selfloops_number_a + selfloops_number_b,
-                        selfloops_number_unique_a + selfloops_number_unique_b,
-                    )
-                },
+                Info::default,
+                |a, b| a + b,
             );
 
         let mut cache = unsafe { &mut (*self.cache.get()) };
-        cache.selfloops_number = Some(selfloops_number);
-        cache.selfloops_number_unique = Some(selfloops_number_unique);
+        cache.selfloops_number = Some(info.selfloops_number);
+        cache.selfloops_number_unique = Some(info.selfloops_number_unique);
+        cache.singleton_nodes_with_selfloops_number = Some(info.singleton_nodes_with_selfloops_number);
     }
 
     cached_property!(get_selfloops_number, EdgeT, compute_selfloops_number, selfloops_number,
@@ -181,6 +211,16 @@ impl Graph {
         ///```rust
         /// # let graph = graph::test_utilities::load_ppi(true, true, true, true, false, false);
         /// println!("The number of unique self-loops in the graph is  {}", graph.get_unique_selfloop_number());
+        /// ```
+    );
+
+    cached_property!(get_singleton_nodes_with_selfloops_number, NodeT, compute_selfloops_number, singleton_nodes_with_selfloops_number,  
+        /// Returns number of singleton nodes with self-loops within the graph.
+        ///
+        /// # Example
+        ///```rust
+        /// # let graph = graph::test_utilities::load_ppi(true, true, true, true, false, false);
+        /// println!("The graph contains {} singleton nodes with self-loops", graph.get_singleton_nodes_with_selfloops_number());
         /// ```
     );
 }
