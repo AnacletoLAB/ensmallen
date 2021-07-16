7a2c48b23a43ae57ae1fbf2f5020d6f8304105b3
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2021-07-16 09:52:11 +0300

    Testing remap using indexed parallell iterators

--- graph/src/remap.rs
+++ graph/src/remap.rs
@@ -226,8 +226,8 @@ impl Graph {
 
         build_graph_from_integers(
             Some(
-                self.par_iter_edge_node_names_and_edge_type_name_and_edge_weight(true)
-                    //.progress_with(pb)
+                self.par_iter_directed_edge_node_names_and_edge_type_name_and_edge_weight()
+                    .progress_with(pb)
                     .map(
                         |(edge_id, _, src_name, _, dst_name, _, edge_type, weight)| unsafe {
                             (
