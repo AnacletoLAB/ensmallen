4aaff2268a1f224cd747902a91c769e03d81e37b
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2021-07-16 10:52:14 +0300

    Made sorting faster

--- graph/src/sort.rs
+++ graph/src/sort.rs
@@ -1,5 +1,9 @@
+use rayon::iter::IndexedParallelIterator;
+use rayon::iter::IntoParallelIterator;
+use rayon::iter::ParallelIterator;
+use rayon::slice::ParallelSliceMut;
+
 use super::*;
-use permutation::permutation;
 
 /// # Sorting algorithms.
 impl Graph {
@@ -8,10 +12,20 @@ impl Graph {
         if self.has_nodes_sorted_by_increasing_outbound_node_degree() {
             return self.clone();
         }
-        let sorted_node_ids_permutation = permutation::sort(self.get_node_degrees());
-        let new_sorted_node_ids =
-            sorted_node_ids_permutation.apply_slice(self.iter_node_ids().collect::<Vec<NodeT>>());
-        unsafe { self.remap_unchecked_from_node_ids(new_sorted_node_ids) }
+        let mut node_ids_and_node_degrees =
+            vec![(0 as usize, 0 as NodeT); self.get_nodes_number() as usize];
+        self.par_iter_node_degrees()
+            .enumerate()
+            .collect_into_vec(&mut node_ids_and_node_degrees);
+        node_ids_and_node_degrees.par_sort_unstable_by(|(_, node_degree_a), (_, node_degree_b)| {
+            node_degree_a.cmp(node_degree_b)
+        });
+        let mut new_node_ids = vec![0 as NodeT; self.get_nodes_number() as usize];
+        node_ids_and_node_degrees
+            .into_par_iter()
+            .map(|(node_id, _)| node_id as NodeT)
+            .collect_into_vec(&mut new_node_ids);
+        unsafe { self.remap_unchecked_from_node_ids(new_node_ids) }
     }
 
     /// Returns graph with node IDs sorted by decreasing outbound node degree.
@@ -19,10 +33,19 @@ impl Graph {
         if self.has_nodes_sorted_by_decreasing_outbound_node_degree() {
             return self.clone();
         }
-        let sorted_node_ids_permutation =
-            permutation::sort_by(self.get_node_degrees(), |a, b| b.cmp(a));
-        let new_sorted_node_ids =
-            sorted_node_ids_permutation.apply_slice(self.iter_node_ids().collect::<Vec<NodeT>>());
-        unsafe { self.remap_unchecked_from_node_ids(new_sorted_node_ids) }
+        let mut node_ids_and_node_degrees =
+            vec![(0 as usize, 0 as NodeT); self.get_nodes_number() as usize];
+        self.par_iter_node_degrees()
+            .enumerate()
+            .collect_into_vec(&mut node_ids_and_node_degrees);
+        node_ids_and_node_degrees.par_sort_unstable_by(|(_, node_degree_a), (_, node_degree_b)| {
+            node_degree_b.cmp(node_degree_a)
+        });
+        let mut new_node_ids = vec![0 as NodeT; self.get_nodes_number() as usize];
+        node_ids_and_node_degrees
+            .into_par_iter()
+            .map(|(node_id, _)| node_id as NodeT)
+            .collect_into_vec(&mut new_node_ids);
+        unsafe { self.remap_unchecked_from_node_ids(new_node_ids) }
     }
 }
