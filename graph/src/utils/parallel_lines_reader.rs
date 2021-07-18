c1c9200fd1579ca1866d9c3e6c38bd89c0a8ad4f
TommasoFontana <tommaso.fontana.96@gmail.com>
2021-07-18 07:28:25 +0300

    added features to parallel lines reader

--- graph/src/utils/parallel_lines_reader.rs
+++ graph/src/utils/parallel_lines_reader.rs
@@ -1,3 +1,4 @@
+use super::*;
 use rayon::iter::plumbing::{UnindexedProducer, bridge_unindexed};
 use rayon::prelude::*;
 use std::fs::File;
@@ -10,37 +11,67 @@ const LINE_LENGTH_EXTIMATE: usize = 256;
 pub struct ParallelLines<'a>{
     path: &'a str,
     number_of_lines: Option<usize>,
+    number_of_rows_to_skip: Option<usize>,
 }
 
 impl<'a> ParallelLines<'a> {
-    pub fn new(path: &str) -> ParallelLines {
-        ParallelLines{
+    pub fn new(path: &str) -> Result<ParallelLines> {
+
+        match File::open(path.clone()) {
+            Ok(_) => Ok(()),
+            Err(_) => Err(format!("Cannot open file {}", path)),
+        }?;
+
+        Ok(ParallelLines{
             path,
             number_of_lines: None,
-        }
+            number_of_rows_to_skip: None,
+        })
     }
 
-    pub fn with_capacity(path: &str, number_of_lines: usize) -> ParallelLines {
-        ParallelLines{
+    pub fn with_capacity(path: &str, number_of_lines: usize) -> Result<ParallelLines>  {
+        
+        match File::open(path.clone()) {
+            Ok(_) => Ok(()),
+            Err(_) => Err(format!("Cannot open file {}", path)),
+        }?;
+
+        Ok(ParallelLines{
             path,
             number_of_lines: Some(number_of_lines),
-        }
+            number_of_rows_to_skip: None,
+        })
+    }
+
+    pub fn skip_rows(&mut self, number_of_rows_to_skip: usize) {
+        self.number_of_rows_to_skip = Some(number_of_rows_to_skip);
     }
 }
 
 
 impl<'a> ParallelIterator for ParallelLines<'a> {
-    type Item = String;
+    type Item = std::io::Result<String>;
 
     fn drive_unindexed<C>(self, consumer: C) -> C::Result
     where
             C: rayon::iter::plumbing::UnindexedConsumer<Self::Item> {
 
         let file = File::open(self.path.clone()).unwrap();
+
+        let file_len  =file.metadata().unwrap().len();
+        let mut buff_reader = BufReader::with_capacity(READER_CAPACITY,file);
+
+        if let Some(rts) = self.number_of_rows_to_skip {
+            for _ in 0..rts {
+                let mut _buffer = String::new();
+                buff_reader.read_line(&mut _buffer).unwrap();
+            }
+        }
+
         bridge_unindexed(ParallelLinesProducer{
             path: self.path,
-            max: file.metadata().unwrap().len(),
-            file: BufReader::with_capacity(READER_CAPACITY,file), 
+            max: file_len,
+            file: buff_reader, 
         }, consumer)
     }
 
@@ -56,27 +87,34 @@ struct ParallelLinesProducer<'a> {
 }
 
 impl<'a> Iterator for ParallelLinesProducer<'a> {
-    type Item = String;
+    type Item = std::io::Result<String>;
 
     fn next(&mut self) -> Option<Self::Item> {
-        if self.file.stream_position().unwrap() >= self.max {
+        let pos = match self.file.stream_position() {
+            Ok(pos) => pos,
+            Err(e) => return Some(Err(e))
+        };
+        if pos >= self.max {
             return None;
         }
         let mut result = String::with_capacity(LINE_LENGTH_EXTIMATE);
-        let chars_read = self.file.read_line(&mut result).unwrap();
+        let chars_read = match self.file.read_line(&mut result) {
+            Ok(result) => result,
+            Err(e) => return Some(Err(e)),
+        };
         if chars_read == 0 {
             None
         } else {
             if result.ends_with('\n') {
                 result.pop().unwrap();
             }
-            Some(result)
+            Some(Ok(result))
         }
     }
 }
 
 impl<'a> UnindexedProducer for ParallelLinesProducer<'a> {
-    type Item = String;
+    type Item = std::io::Result<String>;
 
     /// Split the file in two approximately balanced streams
     fn split(mut self) -> (Self, Option<Self>) {
