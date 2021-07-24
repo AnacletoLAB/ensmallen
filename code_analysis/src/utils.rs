3dc3044ca034b8df267f24fc85447fd49023ebae
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2021-07-24 11:10:57 +0200

    Updated utils in code analysis

--- code_analysis/src/utils.rs
+++ code_analysis/src/utils.rs
@@ -2,7 +2,6 @@ use super::*;
 use std::fs;
 use walkdir::WalkDir;
 
-
 /// List of the files we will skip in the analysis
 /// becasue they have features we don't have implmented yet
 /// nor we care about.
@@ -16,11 +15,11 @@ const BLACKLIST: &[&str] = &[
     "method_caller.rs",
     "parallel_lines_reader.rs",
     "iters_wrapper.rs",
-    "parallel_lines_reader_with_index.rs"
+    "parallel_lines_reader_with_index.rs",
 ];
 
 pub fn skip_file(path: &str) -> bool {
-    for deny in BLACKLIST.iter(){
+    for deny in BLACKLIST.iter() {
         if !path.ends_with(".rs") || path.contains(deny) {
             eprintln!("Skipping the file {}", path);
             return true;
@@ -33,19 +32,18 @@ pub fn skip_file(path: &str) -> bool {
 /// of the library.
 ///
 /// # Panics
-/// If this function panics then probably the source folder path is wrong! 
+/// If this function panics then probably the source folder path is wrong!
 pub fn get_library_sources() -> Vec<Module> {
     let src_files: Vec<String> = WalkDir::new("../graph/src/")
         .into_iter()
         .filter_map(|entry| {
-                let value = entry.unwrap();
-                if value.file_type().is_dir() {
-                    None
-                } else {
-                    Some(value.path().to_str().unwrap().to_string())
-                }
+            let value = entry.unwrap();
+            if value.file_type().is_dir() {
+                None
+            } else {
+                Some(value.path().to_str().unwrap().to_string())
             }
-        )
+        })
         .filter(|path| !skip_file(path))
         .collect();
 
