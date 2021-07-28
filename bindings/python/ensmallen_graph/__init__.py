515f597f0b095f9c52f180e242d3b4c10e950ba0
Tommaso Fontana <tommaso.fontana.96@gmail.com>
2021-07-28 17:18:14 +0200

    added preprocessing to auto-gen of __init__.py

--- bindings/python/ensmallen_graph/__init__.py
+++ bindings/python/ensmallen_graph/__init__.py
@@ -2,5 +2,6 @@
 from .ensmallen_graph import edge_list_utils # pylint: disable=import-error
 from .ensmallen_graph import url_utilities # pylint: disable=import-error
 from .ensmallen_graph import constructors # pylint: disable=import-error
+from .ensmallen_graph import preprocessing # pylint: disable=import-error
 from .ensmallen_graph import EnsmallenGraph # pylint: disable=import-error
-__all__ = ["edge_list_utils", "url_utilities", "constructors", "EnsmallenGraph"]
+__all__ = ["edge_list_utils", "url_utilities", "constructors", "preprocessing", "EnsmallenGraph"]    
