40fd2ae11d1a2f2571c75f014fcc1e56332dde6d
Tommaso Fontana <tommaso.fontana.96@gmail.com>
2021-08-03 09:53:42 +0200

    added new parallel_lines_reader_with_index

--- graph/src/utils/parallel_lines_reader_with_index.rs
+++ graph/src/utils/parallel_lines_reader_with_index.rs
@@ -1,116 +1,51 @@
-use rayon::current_num_threads;
 use rayon::iter::plumbing::{bridge_unindexed, UnindexedProducer};
 use rayon::prelude::*;
-use std::cell::UnsafeCell;
-use std::convert::TryInto;
 use std::fs::File;
-use std::intrinsics::unlikely;
-use std::io::{prelude::*, BufReader};
-use std::sync::atomic::{AtomicBool, AtomicUsize, Ordering};
-use std::sync::{Arc, RwLock};
-use std::thread;
-use std::time::Duration;
+use std::io::{prelude::*, BufReader, SeekFrom};
+use std::sync::{Arc, Mutex};
+#[cfg(target_os = "linux")]
+use nix::fcntl::*;
+#[cfg(target_os = "linux")]
+use std::os::unix::io::AsRawFd;
 
-const READER_CAPACITY: usize = 8 * 1024 * 1024;
-const BUFFER_SIZE: usize = 64;
-
-fn lines_reader(
-    reader: BufReader<File>,
-    producers: Arc<RwLock<Vec<Arc<SPSCRingBuffer>>>>,
-    comment_symbol: Option<String>,
-) {
-    let mut cells_to_populate_number: usize = 0;
-    let mut write_index: usize = 0;
-    let maximum_producers_number = current_num_threads();
-    let mut locked_producers = None;
-    let mut producer: Arc<SPSCRingBuffer> = {
-        let producers = producers.read().unwrap();
-        producers[0].clone()
-    };
-
-    for (line_number, line) in reader
-        .lines()
-        .map(|line| match line {
-            Ok(l) => Ok(l),
-            Err(e) => Err(e.to_string()),
-        })
-        .filter(move |line| match (line, &comment_symbol) {
-            (Ok(line), Some(cs)) => !line.is_empty() && !line.starts_with(cs),
-            (Ok(line), _) => !line.is_empty(),
-            _ => true,
-        })
-        .enumerate()
-    {
-        // If the number of cells to populate is zero,
-        // we need to lock on the threads and find which
-        // thread queue should be filled.
-        'inner: while cells_to_populate_number == 0 {
-            let producers = locked_producers.get_or_insert(producers.read().unwrap());
-            let producers_number = producers.len();
-            for producer_index in (line_number..(line_number + producers_number))
-                .map(|index| index % producers_number)
-            {
-                let (free_cells_number, this_write_index) =
-                    producers[producer_index].get_free_cells_number_and_write_index();
-
-                if free_cells_number != 0 {
-                    producer = producers[producer_index].clone();
-                    cells_to_populate_number = free_cells_number;
-                    write_index = this_write_index;
-                    break 'inner;
-                }
-            }
-            // If some producers still may be created, we cannot
-            // keep this lock, otherwise we can just keep it.
-            if producers_number < maximum_producers_number {
-                locked_producers = None;
-            }
-        }
-        producer.write_unchecked(write_index, (line_number, line));
-        write_index = (write_index + 1) % BUFFER_SIZE;
-        // Surely by the end of this loop, we have
-        // populated a cell.
-        cells_to_populate_number -= 1;
-        // If we have finished populating the cells,
-        // we need to update the write index
-        if cells_to_populate_number == 0 {
-            producer.write_idx.store(write_index, Ordering::SeqCst);
-        }
-    }
-
-    producer.write_idx.store(write_index, Ordering::SeqCst);
-
-    // We free the lock guard.
-    drop(locked_producers);
-
-    let producers = producers.write().unwrap();
-    for producer in producers.iter() {
-        producer.stop();
-    }
-}
+pub const READER_CAPACITY: usize = 16 * 1024;
 
 type IterType = (usize, Result<String, String>);
 
-pub struct ParallelLinesWithIndex {
+pub struct ParallelLinesWithIndex<'a> {
+    path: &'a str,
     file: File,
     comment_symbol: Option<String>,
     number_of_lines: Option<usize>,
     number_of_rows_to_skip: Option<usize>,
+    buffer_size: usize,
+    max_producers: usize,
 }
 
-impl ParallelLinesWithIndex {
-    pub fn new(path: &str) -> Result<ParallelLinesWithIndex, String> {
+impl<'a> ParallelLinesWithIndex<'a> {
+    pub fn new(path: &'a str) -> Result<ParallelLinesWithIndex<'a>, String> {
         let file = match File::open(path.clone()) {
             Ok(file) => Ok(file),
             Err(_) => Err(format!("Cannot open file {}", path)),
         }?;
 
         Ok(ParallelLinesWithIndex {
+            path: path,
             file,
             number_of_lines: None,
             comment_symbol: None,
             number_of_rows_to_skip: None,
+            buffer_size: READER_CAPACITY,
+            max_producers: num_cpus::get(),
         })
+    }
+
+    pub fn set_buffer_size(&mut self, buffer_size: usize) {
+        self.buffer_size = buffer_size;
+    }
+
+    pub fn set_max_producers(&mut self, max_producers: usize) {
+        self.max_producers = max_producers;
     }
 
     pub fn set_skip_rows(&mut self, number_of_rows_to_skip: usize) {
@@ -122,27 +57,56 @@ impl ParallelLinesWithIndex {
     }
 }
 
-impl ParallelIterator for ParallelLinesWithIndex {
+impl<'a> ParallelIterator for ParallelLinesWithIndex<'a> {
     type Item = IterType;
 
     fn drive_unindexed<C>(self, consumer: C) -> C::Result
     where
         C: rayon::iter::plumbing::UnindexedConsumer<Self::Item>,
-    {
-        let mut reader = BufReader::with_capacity(READER_CAPACITY, self.file);
+    {   
 
+        #[cfg(target_os = "linux")]
+        let _ = posix_fadvise(
+            self.file.as_raw_fd(),
+            0,
+            0,
+            PosixFadviseAdvice::POSIX_FADV_SEQUENTIAL,
+        );
+
+        // Create the file reader
+        let mut reader = BufReader::with_capacity(
+            self.buffer_size, 
+            self.file
+        );
+        // Skip the first rows (as specified by the user)
         if let Some(rts) = self.number_of_rows_to_skip {
             for _ in 0..rts {
                 let mut _buffer = String::new();
-                reader.read_line(&mut _buffer).unwrap();
+                let result_bytes_read = reader.read_line(&mut _buffer);
+                match result_bytes_read {
+                    Ok(bytes_read) => {
+                        // Reached End Of File
+                        if bytes_read == 0 {
+                            break;
+                        }
+                    }
+                    Err(_errot) => {}
+                }
             }
         }
 
-        let buffers = Arc::new(RwLock::new(vec![]));
-        let producer = ParalellLinesProducerWithIndex::new(buffers.clone()).unwrap();
-
-        let comment_symbol = self.comment_symbol;
-        thread::spawn(|| lines_reader(reader, buffers, comment_symbol));
+        // Create the first producer
+        let producer = ParalellLinesProducerWithIndex{
+            path: self.path,
+            file: reader,
+            line_count: 0,
+            modulus: 0,
+            remainder: 0,
+            max: self.max_producers,
+            prod_count: Arc::new(Mutex::new(1)),
+            buffer_size: self.buffer_size,
+            comment_symbol: self.comment_symbol,
+        };
         bridge_unindexed(producer, consumer)
     }
 
@@ -152,135 +116,140 @@ impl ParallelIterator for ParallelLinesWithIndex {
 }
 
 #[derive(Debug)]
-pub struct SPSCRingBuffer {
-    cells: UnsafeCell<[IterType; BUFFER_SIZE]>,
-    write_idx: AtomicUsize,
-    read_idx: AtomicUsize,
-    stop: AtomicBool,
+struct ParalellLinesProducerWithIndex<'a> {
+    path: &'a str,
+    file: BufReader<File>,
+    line_count: usize,
+    modulus: usize,
+    remainder: usize,
+    max: usize,
+    prod_count: Arc<Mutex<usize>>,
+    buffer_size: usize,
+    comment_symbol: Option<String>,
 }
 
-unsafe impl Sync for SPSCRingBuffer {}
-
-#[macro_export]
-/// Create a vector of atomic using a default value.
-/// the syntax is:
-/// `vec_atomic[AtomicTYPE; DEFAULT_VALUE; SIZE]`
-macro_rules! init_array {
-    [$default_value:expr ; $size:expr] => {{
-        (0..$size)
-            .map(|_| $default_value)
-            .collect::<Vec<_>>().try_into().unwrap()
-    }}
-}
-
-impl SPSCRingBuffer {
-    pub fn new() -> SPSCRingBuffer {
-        SPSCRingBuffer {
-            cells: UnsafeCell::new(init_array![(0, Result::Ok(String::new())); BUFFER_SIZE]),
-            write_idx: AtomicUsize::new(0),
-            read_idx: AtomicUsize::new(0),
-            stop: AtomicBool::new(false),
-        }
-    }
-
-    pub fn read(&self) -> Option<IterType> {
-        let idx = self.read_idx.load(Ordering::SeqCst);
-        while unlikely(idx == self.write_idx.load(Ordering::SeqCst)) {
-            if unlikely(self.is_stopped()) {
-                return None;
-            }
-            std::thread::sleep(Duration::from_micros(10));
-        }
-
-        // get the id
-        // get the str and replace the spot with an empty one
-        let cells = unsafe { &mut *self.cells.get() };
-        // TODO: possible optimizztion using MaybeInit
-        let value = std::mem::replace(&mut cells[idx], (0, Ok(String::new())));
-
-        self.read_idx
-            .store((idx + 1) % BUFFER_SIZE, Ordering::SeqCst);
-
-        Some(value)
-    }
-
-    pub fn stop(&self) {
-        self.stop.store(true, Ordering::SeqCst);
-    }
-
-    pub fn is_stopped(&self) -> bool {
-        self.stop.load(Ordering::SeqCst)
-    }
-
-    pub fn len(&self) -> usize {
-        (BUFFER_SIZE + self.write_idx.load(Ordering::SeqCst) - self.read_idx.load(Ordering::SeqCst))
-            % BUFFER_SIZE
-    }
-
-    /// returns how many free cells are **currently** available, the index of the first one.
-    pub fn get_free_cells_number_and_write_index(&self) -> (usize, usize) {
-        let w_idx = self.write_idx.load(Ordering::SeqCst);
-        let r_idx = self.read_idx.load(Ordering::SeqCst);
-        let free_cells_number = (BUFFER_SIZE - w_idx + r_idx - 1) % BUFFER_SIZE;
-        (free_cells_number, w_idx)
-    }
-
-    pub fn write_unchecked(&self, idx: usize, value: IterType) {
-        let cells = unsafe { &mut *self.cells.get() };
-        cells[idx] = value;
+impl<'a> Drop for ParalellLinesProducerWithIndex<'a> {
+    fn drop(&mut self) {
+        let mut count = self.prod_count.lock().unwrap();
+        *count -= 1;
     }
 }
 
-#[derive(Debug)]
-struct ParalellLinesProducerWithIndex {
-    buffers_ref: Arc<RwLock<Vec<Arc<SPSCRingBuffer>>>>,
-    buffer: Arc<SPSCRingBuffer>,
-}
-
-impl ParalellLinesProducerWithIndex {
-    /// initialize and register the producer
-    pub fn new(
-        buffers_ref: Arc<RwLock<Vec<Arc<SPSCRingBuffer>>>>,
-    ) -> Option<ParalellLinesProducerWithIndex> {
-        let buffer = Arc::new(SPSCRingBuffer::new());
-        //debug!("Trying to create a producer");
-        {
-            let mut producers = buffers_ref.write().unwrap();
-            if producers.len() >= current_num_threads() {
-                return None;
-            }
-            //debug!("Created the {}-th producer", producers.len());
-            producers.push(buffer.clone());
-        }
-
-        Some(ParalellLinesProducerWithIndex {
-            buffers_ref,
-            buffer,
-        })
-    }
-}
-
-impl Iterator for ParalellLinesProducerWithIndex {
+impl<'a> Iterator for ParalellLinesProducerWithIndex<'a> {
     type Item = IterType;
 
     fn next(&mut self) -> Option<Self::Item> {
-        self.buffer.read()
+        let mut line = String::with_capacity(128);
+        
+        loop {
+            line.clear();
+
+            // read a line
+            let result_bytes_read = self.file.read_line(&mut line);
+            
+            // check if it's ok, if we reached EOF, and if it's a comment
+            if let Ok(bytes_read) = result_bytes_read {
+                // EOF
+                if bytes_read == 0 {
+                    return None;
+                }
+                // Comment
+                if let Some(cs) = self.comment_symbol.as_ref() {
+                    if line.starts_with(cs){
+                        continue
+                    }
+                }
+            };
+
+            // increase the line count only on non-comment
+            self.line_count += 1;
+
+            // check if we are at the line we want to return
+            if (self.line_count & self.modulus) == self.remainder {
+                if line.ends_with("\n") {
+                    line.pop();
+                    if line.ends_with("\r") {
+                        line.pop();
+                    }
+                }
+
+                return Some((
+                    self.line_count - 1,  
+                    match result_bytes_read {
+                        Ok(_) => Ok(line),
+                        Err(error) => Err(error.to_string()),
+                    }
+                ))
+            }
+
+        }
     }
 }
 
-impl UnindexedProducer for ParalellLinesProducerWithIndex {
+impl<'a> UnindexedProducer for ParalellLinesProducerWithIndex<'a> {
     type Item = IterType;
 
     /// Split the file in two approximately balanced streams
-    fn split(self) -> (Self, Option<Self>) {
+    fn split(mut self) -> (Self, Option<Self>) {
         // Check if it's reasonable to split the stream
-        if self.buffer.is_stopped() {
+        let cond = {
+            let mut count = self.prod_count.lock().unwrap();
+            if *count > self.max {
+                true
+            } else {
+                *count += 1;
+                false
+            }
+        };
+
+        if cond {
             return (self, None);
         }
 
-        let new = ParalellLinesProducerWithIndex::new(self.buffers_ref.clone());
+        let file = File::open(self.path.clone())
+        .expect(&format!("Could not open the file {}", self.path));
 
-        (self, new)
+        #[cfg(target_os = "linux")]
+        let _ = posix_fadvise(
+            file.as_raw_fd(),
+            0,
+            0,
+            PosixFadviseAdvice::POSIX_FADV_SEQUENTIAL,
+        );
+
+        // Create a copy of the file reader of the father
+        let mut new_file = BufReader::with_capacity(
+            self.buffer_size, 
+            file,
+        );
+
+        
+        // Updated its position to the same byte in the file as the father.
+        new_file.seek(
+            SeekFrom::Start(
+                self.file.stream_position()
+                .expect("Could not read the file pointer position in the file.")
+            )
+        ).expect("Could seek the new file to the position of the old one.");
+
+        // Create the child 
+        let new = ParalellLinesProducerWithIndex{
+            path: self.path.clone(),
+            line_count: self.line_count,
+            file: new_file,
+            modulus: (self.modulus << 1) | 1,
+            remainder: self.modulus + self.remainder + 1,
+            max: self.max,
+            buffer_size: self.buffer_size,
+            prod_count: self.prod_count.clone(),
+            comment_symbol: self.comment_symbol.clone(),
+        };
+
+        // Update the father modulus so that the lines are equally splitted
+        self.modulus = (self.modulus << 1) | 1;
+
+        // Returns the two new producers
+        (self, Some(new))
     }
 
     fn fold_with<F>(self, folder: F) -> F
