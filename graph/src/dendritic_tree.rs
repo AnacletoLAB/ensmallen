bb9307087d2fb877f4e963e9fef7010aa5a154b6
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2021-11-26 16:26:23 +0100

    Resolved another corner case in dendric trees

--- graph/src/dendritic_tree.rs
+++ graph/src/dendritic_tree.rs
@@ -222,9 +222,14 @@ impl Graph {
                         // If the neighbour is described as a dentritic leaf that has not
                         // yet a parent, its parent node will be the current node.
                         if neighbour_parent == DENDRITIC_TREE_LEAF
-                            || neighbour_parent == neighbour_node_id && neighbour_node_id > node_id
                         {
                             (*predecessors.value.get())[neighbour_node_id as usize] = node_id;
+                        } else if neighbour_parent == neighbour_node_id {
+                            if neighbour_node_id > node_id {
+                                (*predecessors.value.get())[neighbour_node_id as usize] = node_id;
+                            } else {
+                                (*predecessors.value.get())[node_id as usize] = neighbour_node_id;
+                            }
                         }
                         None
                     }
