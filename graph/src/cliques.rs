c8b8850887950e6315733e30ec78ccb09774ffd6
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2021-12-05 20:39:47 +0100

    Fixed error

--- graph/src/cliques.rs
+++ graph/src/cliques.rs
@@ -124,7 +124,6 @@ impl Graph {
     /// `minimum_degree`: Option<NodeT> - The optional minimum degree, by default 10.
     /// `minimum_clique_size`: Option<NodeT> - The optional minimum clique size, by default 10.
     /// `clique_per_node`: Option<usize> - Maximum number of clique to find for each node.
-    /// `maximum_number_of_cliques`: Option<usize> - Maximum number of cliques to compute. By default, equal to the number of nodes.
     /// `verbose`: Option<bool> - Whether to show a loading bar. By default, True.
     ///
     /// # Raises
@@ -134,7 +133,6 @@ impl Graph {
         minimum_degree: Option<NodeT>,
         minimum_clique_size: Option<NodeT>,
         clique_per_node: Option<usize>,
-        maximum_number_of_cliques: Option<usize>,
         verbose: Option<bool>,
     ) -> Result<impl Iterator<Item = Clique> + '_> {
         self.must_be_undirected()?;
@@ -144,13 +142,9 @@ impl Graph {
         let minimum_clique_size = minimum_clique_size.unwrap_or(10);
         // The number of clique per node to compute, which by default is 1.
         let clique_per_node = clique_per_node.unwrap_or(1);
-        // The maximum number of cliques to compute, which defaults to the number of nodes.
-        let maximum_number_of_cliques =
-            maximum_number_of_cliques.unwrap_or(self.get_nodes_number() as usize);
         // Whether to show the loading bar while computing cliques.
         let verbose = verbose.unwrap_or(true);
         // We create a vector with the initial node degrees of the graph, wrapped into atomic.
-        info!("Create the node degrees vector.");
         let mut node_degrees: Vec<AtomicU32> = Vec::with_capacity(self.get_nodes_number() as usize);
         self.par_iter_node_degrees()
             .map(|degree| AtomicU32::new(degree))
@@ -329,9 +323,6 @@ impl Graph {
             covered_nodes.iter().for_each(|&node_id| {
                 node_degrees_copy[node_id as usize] -= degree;
             });
-            if maximum_number_of_cliques <= clique_roots.len() {
-                break;
-            }
         }
 
         info!(
@@ -448,7 +439,6 @@ impl Graph {
                 minimum_degree,
                 minimum_clique_size,
                 clique_per_node,
-                None,
                 verbose,
             )?
             .collect())
@@ -463,13 +453,7 @@ impl Graph {
         let mut threshold = self.get_maximum_node_degree()? / 10;
         let mut candidate_maximal_clique = loop {
             if let Some(clique) = self
-                .iter_approximated_cliques(
-                    Some(threshold),
-                    Some(threshold),
-                    Some(1),
-                    Some(1),
-                    Some(false),
-                )?
+                .iter_approximated_cliques(Some(threshold), Some(threshold), Some(1), Some(false))?
                 .take(1)
                 .collect::<Vec<Clique>>()
                 .first()
@@ -477,7 +461,7 @@ impl Graph {
                 break clique.to_owned();
             }
             threshold /= 2;
-            if minimum_node_degree > threshold {
+            if minimum_node_degree > threshold || threshold == 1 {
                 return Err("Impossible to detect the maximum clique in this graph.".to_string());
             }
         };
@@ -494,7 +478,6 @@ impl Graph {
                         .unwrap(),
                 ),
                 Some(candidate_maximal_clique.len() + 1),
-                Some(1),
                 Some(1),
                 Some(false),
             )?
@@ -529,7 +512,6 @@ impl Graph {
                 minimum_degree,
                 minimum_clique_size,
                 clique_per_node,
-                None,
                 verbose,
             )?
             .count())
