06e0aea1957f52379e272bde3af375a61b2e86bf
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2022-04-21 00:14:59 +0200

    Exploring speedups in CBOW

--- graph/src/skipgram.rs
+++ graph/src/skipgram.rs
@@ -153,7 +153,7 @@ impl Graph {
             random_state = splitmix64(random_state);
             walk_parameters = walk_parameters.set_random_state(Some(random_state as usize));
 
-            self.iter_complete_walks(&walk_parameters)?
+            self.par_iter_complete_walks(&walk_parameters)?
                 .enumerate()
                 .for_each(|(i, sequence)| {
                     (window_size..(walk_length as usize - window_size)).for_each(|j| {
