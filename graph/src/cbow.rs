21aad174048ba2e1d989a853aed1d66df9554f4e
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2022-04-21 00:21:02 +0200

    Fixed typos

--- graph/src/cbow.rs
+++ graph/src/cbow.rs
@@ -181,7 +181,7 @@ impl Graph {
 
             walk_parameters = walk_parameters.set_random_state(Some(random_state as usize));
 
-            self.par_par_iter_complete_walks(&walk_parameters)?
+            self.par_iter_complete_walks(&walk_parameters)?
                 .enumerate()
                 .for_each(|(i, sequence)| unsafe {
                     (window_size..(walk_length as usize - window_size)).for_each(|j| {
