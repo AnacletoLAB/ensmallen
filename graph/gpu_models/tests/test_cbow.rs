b689d4e6fe29dc73957d5066157c09e21749823d
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2022-04-22 23:31:47 +0200

    Reduced window size

--- graph/gpu_models/tests/test_cbow.rs
+++ graph/gpu_models/tests/test_cbow.rs
@@ -12,7 +12,7 @@ fn test_cbow_on_cora() -> Result<(), GPUError> {
         .unwrap();
     let embedding_size = 64;
     let walks = WalksParameters::new(128).unwrap().set_iterations(Some(1)).unwrap();
-    let cbow = CBOW::new(Some(embedding_size), Some(walks), Some(10), Some(10)).unwrap();
+    let cbow = CBOW::new(Some(embedding_size), Some(walks), Some(4), Some(10)).unwrap();
     let mut embedding = vec![0.0; embedding_size * cora.get_nodes_number() as usize];
     cbow.fit_transform(
         &cora,
