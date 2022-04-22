af6dec12ca58c48fd79ddd5f34b6fa298eb05c79
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2022-04-22 23:01:35 +0200

    Fixed error in test

--- graph/gpu_models/tests/test_cbow.rs
+++ graph/gpu_models/tests/test_cbow.rs
@@ -11,7 +11,7 @@ fn test_cbow_on_cora() -> Result<(), GPUError> {
     cora.enable(Some(true), Some(true), Some(true), Some(false))
         .unwrap();
     let embedding_size = 32;
-    let walks = WalksParameters::new(128).unwrap().set_iterations(Some(10));
+    let walks = WalksParameters::new(128).unwrap().set_iterations(Some(10)).unwrap();
     let cbow = CBOW::new(Some(embedding_size), Some(walks), Some(10), Some(5)).unwrap();
     let mut embedding = vec![0.0; embedding_size * cora.get_nodes_number() as usize];
     cbow.fit_transform(
