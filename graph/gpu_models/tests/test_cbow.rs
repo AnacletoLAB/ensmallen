ddae61995735790aaf938dc98ea0213abde7ea92
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2022-04-22 23:00:51 +0200

    Increased number of iterations

--- graph/gpu_models/tests/test_cbow.rs
+++ graph/gpu_models/tests/test_cbow.rs
@@ -11,7 +11,7 @@ fn test_cbow_on_cora() -> Result<(), GPUError> {
     cora.enable(Some(true), Some(true), Some(true), Some(false))
         .unwrap();
     let embedding_size = 32;
-    let walks = WalksParameters::new(128).unwrap();
+    let walks = WalksParameters::new(128).unwrap().set_iterations(Some(10));
     let cbow = CBOW::new(Some(embedding_size), Some(walks), Some(10), Some(5)).unwrap();
     let mut embedding = vec![0.0; embedding_size * cora.get_nodes_number() as usize];
     cbow.fit_transform(
@@ -32,7 +32,7 @@ fn test_cbow_on_cora() -> Result<(), GPUError> {
     writer
         .write_lines(
             Some(cora.get_nodes_number() as usize),
-            vec!["node_name".to_string(),]
+            vec!["node_name".to_string()]
                 .into_iter()
                 .chain((0..embedding_size).map(|e| e.to_string()))
                 .collect::<Vec<String>>(),
