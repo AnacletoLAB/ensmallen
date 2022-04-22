2531f208eca556740d4d5ce83d080fe6d8a2822d
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2022-04-22 21:24:39 +0200

    Fixed yet another error in test

--- graph/gpu_models/tests/test_cbow.rs
+++ graph/gpu_models/tests/test_cbow.rs
@@ -7,7 +7,7 @@ use graph::test_utilities::*;
 fn test_cbow_on_cora() -> Result<(), GPUError> {
     let mut cora = load_cora();
     let cbow = CBOW::new(Some(128), None, Some(10), Some(5)).unwrap();
-    let embedding = vec![0.0; 128 * cora.get_nodes_number() as usize];
+    let mut embedding = vec![0.0; 128 * cora.get_nodes_number() as usize];
     cbow.fit_transform(&cora, embedding.as_mut_slice(), Some(10), None, Some(32), None);
     Ok(())
 }
