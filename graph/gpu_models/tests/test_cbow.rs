ff94279c51a37ea551aec02e56903090ee8d278d
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2022-04-22 21:27:10 +0200

    Added enables

--- graph/gpu_models/tests/test_cbow.rs
+++ graph/gpu_models/tests/test_cbow.rs
@@ -6,6 +6,7 @@ use graph::test_utilities::*;
 #[test]
 fn test_cbow_on_cora() -> Result<(), GPUError> {
     let mut cora = load_cora();
+    cora.enable(Some(true), Some(true), Some(true));
     let cbow = CBOW::new(Some(128), None, Some(10), Some(5)).unwrap();
     let mut embedding = vec![0.0; 128 * cora.get_nodes_number() as usize];
     cbow.fit_transform(&cora, embedding.as_mut_slice(), Some(10), None, Some(32), None)?;
