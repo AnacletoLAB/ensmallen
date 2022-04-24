e478a2d2ba33139070bc7a12028a3c7128e44f9e
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2022-04-24 15:22:38 +0200

    Increase again batch size

--- graph/cpu_models/tests/test_cbow.rs
+++ graph/cpu_models/tests/test_cbow.rs
@@ -15,14 +15,14 @@ fn test_cbow_on_cora() -> Result<(), String> {
         .unwrap()
         .set_iterations(Some(1))
         .unwrap();
-    let cbow = CBOW::new(Some(embedding_size), Some(walks), Some(4), Some(5), None).unwrap();
+    let cbow = CBOW::new(Some(embedding_size), Some(walks), Some(4), Some(10), None).unwrap();
     let mut embedding = vec![0.0; embedding_size * cora.get_nodes_number() as usize];
     cbow.fit_transform(
         &cora,
         embedding.as_mut_slice(),
-        Some(3),
+        Some(10),
         None,
-        Some(32),
+        Some(256),
         None,
     )?;
 
