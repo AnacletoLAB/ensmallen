5d8c8fa0b40afc52954faf82b4a249ab13780906
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2022-04-25 19:58:55 +0200

    Updated kernel for CBOW

--- graph/cuda_kernels/src/cbow.rs
+++ graph/cuda_kernels/src/cbow.rs
@@ -7,11 +7,12 @@ use crate::*;
 ///
 pub unsafe extern "ptx-kernel" fn compute_cbow_mini_batch(
     embedding: *mut f32,
-    hidden: *mut f32,
+    total_contexts: *mut f32,
+    contexts_gradient: *mut f32,
     random_walks: *const u32,
     negative_node_ids: *const u32,
     learning_rate: f32,
-    window_size: isize,
+    window_size: usize,
     number_of_negative_samples: usize,
     random_walk_length: usize,
     embedding_size: usize,
@@ -24,146 +25,149 @@ pub unsafe extern "ptx-kernel" fn compute_cbow_mini_batch(
 
     // Embedding has shape (vocabulary_size, embedding_size)
     let embedding = core::slice::from_raw_parts_mut(embedding, vocabulary_size * embedding_size);
-    // Hidden layer has shape (vocabulary_size, embedding_size), same as embedding
-    let hidden = core::slice::from_raw_parts_mut(hidden, vocabulary_size * embedding_size);
     // Random walks matrix has shape (batch_size * iterations, random_walk_length)
     let number_of_random_walks = batch_size * iterations;
-    let number_of_contexts_per_random_walk =
-        (random_walk_length as isize - window_size * 2) as usize;
+    let number_of_contexts_per_random_walk = (random_walk_length - window_size * 2) as usize;
     let random_walks =
         core::slice::from_raw_parts(random_walks, number_of_random_walks * random_walk_length);
+    let total_contexts =
+        core::slice::from_raw_parts_mut(total_contexts, number_of_random_walks * embedding_size);
+    let contexts_gradient =
+        core::slice::from_raw_parts_mut(contexts_gradient, number_of_random_walks * embedding_size);
+
+    let random_walk = &random_walks
+        [random_walk_number * random_walk_length..(random_walk_number + 1) * random_walk_length];
+
+    let total_contexts = &mut total_contexts
+        [random_walk_number * embedding_size..(random_walk_number + 1) * embedding_size];
+
+    let contexts_gradient = &mut contexts_gradient
+        [random_walk_number * embedding_size..(random_walk_number + 1) * embedding_size];
+
     // Negative node IDs have shape (batch_size * iterations * (random_walk_length - window_size * 2), )
     let negative_node_ids = core::slice::from_raw_parts(
         negative_node_ids,
         number_of_negative_samples
             * batch_size
             * iterations
-            * (random_walk_length as isize - window_size * 2) as usize,
+            * (random_walk_length - window_size * 2) as usize,
     );
+
+    let negative_node_ids = &negative_node_ids[random_walk_number
+        * number_of_contexts_per_random_walk
+        * number_of_negative_samples
+        ..(random_walk_number + 1)
+            * number_of_contexts_per_random_walk
+            * number_of_negative_samples];
+
     let scale_factor = (embedding_size as f32).sqrt();
     let context_size = (window_size * 2) as f32;
 
-    // We iterate for all skipgram batches of the random walk.
-    for center in window_size..(random_walk_length as isize - window_size) {
-        // BEGIN THE CONTRACTION STEP FOR THIS BATCH
+    // Create the closure to apply a gradient to a provided node's embedding
+    let weighted_vector_sum = |vector: &mut [f32], variation: &[f32], weight: f32| {
+        vector.iter_mut().zip(variation.iter().cloned()).for_each(
+            |(feature, gradient_feature): (&mut f32, f32)| {
+                *feature += weight * gradient_feature;
+            },
+        );
+    };
 
-        // We retrieve the value of the current central node ID
-        let current_central_node_id =
-            random_walks[random_walk_number * random_walk_length + center as usize] as usize;
-        // and we retrieve its embedding
-        let current_central_node_embedding = &mut hidden[current_central_node_id * embedding_size
-            ..(current_central_node_id + 1) * embedding_size];
-        // We iterate on the context around the center
-        let mut dot: f32 = 0.0;
-        // We compute the dot product of the sum of the contextual node embedding and the
-        // current central node.
-        for context in (-window_size..0).chain(1..window_size + 1) {
-            let current_context_node_id = random_walks
-                [random_walk_number * random_walk_length + (center + context) as usize]
-                as usize;
-            let current_context_node_embedding = &mut embedding[current_context_node_id
-                * embedding_size
-                ..(current_context_node_id + 1) * embedding_size];
-            for feature in 0..embedding_size {
-                dot += current_central_node_embedding[feature]
-                    * current_context_node_embedding[feature];
-            }
-        }
-        // Adjust dot by the size of the embedding to obtain mean.
-        dot /= context_size * scale_factor;
+    let compute_mini_batch_step = |total_context_embedding: &[f32],
+                                   context_embedding_gradient: &mut [f32],
+                                   node_embedding: &mut [f32],
+                                   label: f32| {
+        let dot = node_embedding
+            .iter()
+            .cloned()
+            .zip(total_context_embedding.iter().cloned())
+            .map(|(node_feature, contextual_feature)| node_feature * contextual_feature)
+            .sum::<f32>()
+            / context_size
+            / scale_factor;
 
-        if dot > 10.0 || dot < -10.0{
-            continue;
+        if dot > 20.0 || dot < -20.0 {
+            return;
         }
 
-        // We compute the exponentiation of the dot product.
-        let exponentiated_dot = dot.exp2();
-        // We compute the loss for the POSITIVE node
-        let loss = (1.0
-            - (exponentiated_dot / ((exponentiated_dot + 1.0) * (exponentiated_dot + 1.0))))
-            * learning_rate;
-        // Analogously, since we are not dividing the contexts by context size,
-        // we need to divide the loss by the context size for the update of the
-        // hidden layer.
-        let averaged_context_loss = loss / context_size;
+        let exp_dot = dot.exp2();
+        let loss = (label - exp_dot / ((exp_dot + 1.0) * (exp_dot + 1.0))) * learning_rate;
 
-        // We backpropagate the loss to the hidden layer and the embeddding layer
-        for context in (-window_size..0).chain(1..window_size + 1) {
-            let current_context_node_id = random_walks
-                [random_walk_number * random_walk_length + (center + context) as usize]
-                as usize;
-            let current_context_node_embedding = &mut embedding[current_context_node_id
-                * embedding_size
-                ..(current_context_node_id + 1) * embedding_size];
-            for feature in 0..embedding_size {
-                current_central_node_embedding[feature] +=
-                    current_context_node_embedding[feature] * averaged_context_loss;
-                current_context_node_embedding[feature] +=
-                    current_central_node_embedding[feature] * loss;
-            }
-        }
+        weighted_vector_sum(node_embedding, total_context_embedding, loss / context_size);
+        weighted_vector_sum(context_embedding_gradient, node_embedding, loss);
+    };
 
-        // BEGIN THE RELAXATION STEP FOR THE NEGATIVES OF THIS CONTEXT
+    // We start to compute the new gradients.
+    (window_size..random_walk_length - window_size)
+        .map(|central_index| {
+            (
+                &random_walk[(central_index - window_size)..central_index],
+                &random_walk[(central_index + 1)..(central_index + window_size)],
+                random_walk[central_index],
+            )
+        })
+        .zip(negative_node_ids.chunks(number_of_negative_samples))
+        .for_each(
+            |((left_context, right_context, central_node_id), negative_node_ids)| {
+                // We compute the total context embedding.
+                total_contexts.iter_mut().for_each(|value| {
+                    *value = 0.0;
+                });
 
-        let start_negatives = (number_of_contexts_per_random_walk * random_walk_number
-            + (center - window_size) as usize)
-            * number_of_negative_samples;
-        let end_negatives = (number_of_contexts_per_random_walk * random_walk_number
-            + (center - window_size) as usize
-            + 1)
-            * number_of_negative_samples;
-        for negative_number in start_negatives..end_negatives {
-            // We retrieve the value of the current central node ID
-            let current_negative_node_id = negative_node_ids[negative_number] as usize;
-            // and we retrieve its embedding
-            let current_negative_node_embedding = &mut hidden[current_negative_node_id
-                * embedding_size
-                ..(current_negative_node_id + 1) * embedding_size];
+                contexts_gradient.iter_mut().for_each(|value| {
+                    *value = 0.0;
+                });
 
-            // We iterate on the context around the center
-            let mut dot: f32 = 0.0;
-            // We compute the dot product of the sum of the contextual node embedding and the
-            // current central node.
-            for context in (-window_size..0).chain(1..window_size + 1) {
-                let current_context_node_id = random_walks
-                    [random_walk_number * random_walk_length + (center + context) as usize]
-                    as usize;
-                let current_context_node_embedding = &mut embedding[current_context_node_id
-                    * embedding_size
-                    ..(current_context_node_id + 1) * embedding_size];
-                for feature in 0..embedding_size {
-                    dot += current_negative_node_embedding[feature]
-                        * current_context_node_embedding[feature];
-                }
-            }
-            // Adjust dot by the size of the embedding to obtain mean.
-            dot /= context_size * scale_factor;
-            // We compute the exponentiation of the dot product.
-            let exponentiated_dot = dot.exp2();
-            // We compute the loss for the NEGATIVE node
-            let loss = (exponentiated_dot
-                / ((exponentiated_dot + 1.0) * (exponentiated_dot + 1.0)))
-                * learning_rate;
-            // Analogously, since we are not dividing the contexts by context size,
-            // we need to divide the loss by the context size for the update of the
-            // hidden layer.
-            let averaged_context_loss = loss / context_size;
+                // Then we sum over it the other values.
+                left_context
+                    .iter()
+                    .chain(right_context.iter())
+                    .for_each(|contextual_node_id| {
+                        let contextual_node_id = *contextual_node_id as usize;
+                        embedding[(contextual_node_id * embedding_size)
+                            ..((contextual_node_id + 1) * embedding_size)]
+                            .iter()
+                            .zip(total_contexts.iter_mut())
+                            .for_each(|(feature, total_feature)| {
+                                *total_feature += *feature;
+                            });
+                    });
 
-            // We backpropagate the loss to the hidden layer and the embeddding layer
-            for context in (-window_size..0).chain(1..window_size + 1) {
-                let current_context_node_id = random_walks
-                    [random_walk_number * random_walk_length + (center + context) as usize]
-                    as usize;
-                let current_context_node_embedding = &mut embedding[current_context_node_id
-                    * embedding_size
-                    ..(current_context_node_id + 1) * embedding_size];
-                for feature in 0..embedding_size {
-                    current_negative_node_embedding[feature] -=
-                        current_context_node_embedding[feature] * averaged_context_loss;
-                    current_context_node_embedding[feature] -=
-                        current_negative_node_embedding[feature] * loss;
-                }
-            }
-        }
-    }
+                // We now compute the gradient relative to the positive
+                compute_mini_batch_step(
+                    total_contexts,
+                    contexts_gradient,
+                    &mut embedding[((central_node_id as usize) * embedding_size)
+                        ..(((central_node_id as usize) + 1) * embedding_size)],
+                    1.0,
+                );
+
+                // We compute the gradients relative to the negative classes.
+                negative_node_ids
+                    .iter()
+                    .cloned()
+                    .filter(|non_central_node_id| *non_central_node_id != central_node_id)
+                    .for_each(|non_central_node_id| {
+                        compute_mini_batch_step(
+                            total_contexts,
+                            contexts_gradient,
+                            &mut embedding[((non_central_node_id as usize) * embedding_size)
+                                ..(((non_central_node_id as usize) + 1) * embedding_size)],
+                            0.0,
+                        );
+                    });
+                left_context
+                    .iter()
+                    .chain(right_context.iter())
+                    .cloned()
+                    .for_each(|contextual_node_id| {
+                        let contextual_node_id = contextual_node_id as usize;
+                        weighted_vector_sum(
+                            &mut embedding[(contextual_node_id * embedding_size)
+                                ..((contextual_node_id + 1) * embedding_size)],
+                            contexts_gradient,
+                            1.0,
+                        );
+                    });
+            },
+        );
 }
