f79bf870d86e79ceb9ae09999ea349151911ff13
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2022-04-25 10:51:24 +0200

    Testing something stupid about gradient

--- graph/cpu_models/src/cbow.rs
+++ graph/cpu_models/src/cbow.rs
@@ -458,7 +458,7 @@ impl CBOW {
                             .iter_mut()
                             .zip(gradient.iter())
                             .for_each(|(hidden_feature, gradient_feature): (&mut f32, &f32)| {
-                                *hidden_feature += *gradient_feature;
+                                *hidden_feature -= *gradient_feature;
                             });
                     }
                 };
@@ -472,7 +472,7 @@ impl CBOW {
                             .iter_mut()
                             .zip(gradient.iter())
                             .for_each(|(embedding_feature, gradient_feature): (&mut f32, &f32)| {
-                                *embedding_feature += *gradient_feature;
+                                *embedding_feature -= *gradient_feature;
                             });
                     }
                 };
