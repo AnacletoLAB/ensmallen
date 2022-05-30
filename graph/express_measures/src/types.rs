699017979e241fce679de6bd372eb4fb7d9ff454
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2022-05-30 17:47:08 +0200

    Fixed missing requirements

--- graph/express_measures/src/types.rs
+++ graph/express_measures/src/types.rs
@@ -1,8 +1,8 @@
 use core::fmt::Debug;
 use num_traits::{Float, Unsigned};
 
-pub trait ThreadFloat: Float + Send + Sync + Copy {}
+pub trait ThreadFloat: Float + Send + Sync + Copy + std::iter::Sum {}
 pub trait ThreadUnsigned: Unsigned + Send + Sync + Copy + TryInto<usize> + Debug {}
 
-impl<T> ThreadFloat for T where T: Float + Send + Sync + Copy {}
+impl<T> ThreadFloat for T where T: Float + Send + Sync + Copy  + std::iter::Sum {}
 impl<T> ThreadUnsigned for T where T: Unsigned + Send + Sync + Copy + TryInto<usize> + Debug {}
