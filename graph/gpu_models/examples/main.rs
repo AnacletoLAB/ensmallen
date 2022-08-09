b2a4b3e7252e7aa235ee7c59194e5f31b5bb39f0
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2022-08-09 23:23:37 +0200

    Updated test

--- graph/gpu_models/examples/main.rs
+++ graph/gpu_models/examples/main.rs
@@ -16,22 +16,22 @@ pub fn add_one(vals: &[f32]) -> Result<Vec<f32>, GPUError> {
     // get a function from the compiled code
     let kernel = ptx.get_kernel("add_one")?;
     
-    // allocate a gpu buffer and copy data from the host
-    let buffer = gpu.buffer_from_slice::<f32>(vals)?;
-    // set the parallelizzation specs
-    let grid = Grid::default().set_block_x(1024)?;
+    // // allocate a gpu buffer and copy data from the host
+    // let buffer = gpu.buffer_from_slice::<f32>(vals)?;
+    // // set the parallelizzation specs
+    // let grid = Grid::default().set_block_x(1024)?;
 
-    // launch the function with the args
-    gpu.launch_kernel(&kernel, &grid, args![
-        buffer.as_device_ptr(),
-        buffer.len(),
-    ])?;
-    // wait for the gpu to finish
-    gpu.synchronize()?;
+    // // launch the function with the args
+    // gpu.launch_kernel(&kernel, &grid, args![
+    //     buffer.as_device_ptr(),
+    //     buffer.len(),
+    // ])?;
+    // // wait for the gpu to finish
+    // gpu.synchronize()?;
 
-    // copy the results back from the gpu to the host
-    let result = buffer.to_vec()?;
-    Ok(result)
+    // // copy the results back from the gpu to the host
+    // let result = buffer.to_vec()?;
+    Ok(vec![0.0])
 }
 
 fn main() {
