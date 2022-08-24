b5fc2315b79a3a5d81b7753d09f0d6850b444e6c
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2022-08-24 11:04:10 +0200

    Added loading bars

--- graph/cpu_models/src/wine.rs
+++ graph/cpu_models/src/wine.rs
@@ -5,6 +5,7 @@ use crate::{
 use core::sync::atomic::Ordering;
 use ensmallen_traits::prelude::*;
 use graph::{Graph, NodeT};
+use indicatif::{ParallelProgressIterator, ProgressBar, ProgressStyle};
 use rayon::prelude::*;
 use vec_rand::{splitmix64, IteratorSuss};
 
@@ -42,6 +43,10 @@ impl BasicWINE {
 
     pub fn get_basic_inferred_node_embedding(&self) -> &BasicAnchorsInferredNodeEmbedding {
         &self.baine
+    }
+
+    pub fn is_verbose(&self) -> bool {
+        self.baine.is_verbose()
     }
 }
 
@@ -85,16 +90,42 @@ where
         let mut random_walk_length: Feature = Feature::ZERO;
         let mut random_state = splitmix64(self.get_random_state());
 
+        // Depending whether verbosity was requested by the user
+        // we create or not a visible progress bar to show the progress
+        // in the computation of the features.
+        let depth_progress = if self.get_basic_wine().is_verbose() {
+            let pb = ProgressBar::new(self.get_walk_length() as u64);
+            pb.set_style(ProgressStyle::default_bar().template(concat!(
+                "depth {spinner:.green} [{elapsed_precise}] ",
+                "[{bar:40.cyan/blue}] ({pos}/{len}, ETA {eta})"
+            )));
+            pb
+        } else {
+            ProgressBar::hidden()
+        };
+
         // Until the bucket is not empty we start to iterate.
         let max_depth = Feature::try_from(self.get_walk_length()).unwrap_or(Feature::MAX);
         while !bucket.is_empty() {
             random_walk_length += Feature::ONE;
             random_state = splitmix64(random_state);
 
+            let frontier_progress = if self.get_basic_wine().is_verbose() {
+                let pb = ProgressBar::new(bucket.len() as u64);
+                pb.set_style(ProgressStyle::default_bar().template(concat!(
+                    "frontier {spinner:.green} [{elapsed_precise}] ",
+                    "[{bar:40.cyan/blue}] ({pos}/{len}, ETA {eta})"
+                )));
+                pb
+            } else {
+                ProgressBar::hidden()
+            };
+
             if random_walk_length < max_depth {
                 // We compute the next bucket of nodes, i.e. the next step of the frontier.
                 bucket = bucket
                     .into_par_iter()
+                    .progress_with(frontier_progress)
                     .flat_map_iter(|node_id| {
                         graph
                             .iter_unchecked_neighbour_node_ids_from_source_node_id(node_id)
@@ -106,16 +137,21 @@ where
                             })
                     })
                     .collect::<Vec<NodeT>>();
+                depth_progress.inc(1);
             } else {
                 // We compute the next bucket of nodes, i.e. the next step of the frontier.
-                bucket.into_par_iter().for_each(|node_id| {
-                    graph
-                        .iter_unchecked_neighbour_node_ids_from_source_node_id(node_id)
-                        .for_each(|neighbour_node_id| {
-                            shared_features[neighbour_node_id as usize]
-                                .fetch_add(Feature::ONE, Ordering::Relaxed);
-                        });
-                });
+                bucket
+                    .into_par_iter()
+                    .progress_with(frontier_progress)
+                    .for_each(|node_id| {
+                        graph
+                            .iter_unchecked_neighbour_node_ids_from_source_node_id(node_id)
+                            .for_each(|neighbour_node_id| {
+                                shared_features[neighbour_node_id as usize]
+                                    .fetch_add(Feature::ONE, Ordering::Relaxed);
+                            });
+                    });
+                depth_progress.inc(1);
                 return;
             }
         }
