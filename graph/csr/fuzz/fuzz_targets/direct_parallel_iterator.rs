ee994e9621e691deff47172fc4e854c46280df53
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2023-06-03 12:49:49 +0200

    Simplified fuzz case

--- graph/csr/fuzz/fuzz_targets/direct_parallel_iterator.rs
+++ graph/csr/fuzz/fuzz_targets/direct_parallel_iterator.rs
@@ -11,7 +11,7 @@ struct FuzzCase {
     // We use u8 so it cannot create
     // an object that is too big
     edges: Vec<(u8, u8)>,
-    number_of_nodes: u8,
+    number_of_trailing_singletons: u8,
 }
 
 fuzz_target!(|data: FuzzCase| {
@@ -28,7 +28,7 @@ fuzz_target!(|data: FuzzCase| {
         .max()
         .unwrap_or(0) as usize
         + 1)
-    .max(data.number_of_nodes as usize);
+        + data.number_of_trailing_singletons as usize;
 
     // Build the CSR
     let csrb = ConcurrentCSRBuilder::new(edges.len() as u64, number_of_nodes as u32);
@@ -44,7 +44,10 @@ fuzz_target!(|data: FuzzCase| {
     let csr = csrb.build();
 
     // Check that the number of nodes is correct
-    assert_eq!(csr.get_number_of_nodes(), number_of_nodes as u32);
+    assert_eq!(
+        csr.get_number_of_nodes(),
+        number_of_nodes as u32
+    );
 
     // Check that the number of edges is correct
     assert_eq!(csr.get_number_of_directed_edges(), edges.len() as u64);
