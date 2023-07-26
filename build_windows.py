5145d6ceaff3a6f3fa447417acfd62df34216f20
Fontana Tommaso <tommaso.fontana.ext@wuerth-phoenix.com>
2023-07-26 22:00:21 +0200

    cleaning up the crate and making a workspace for easier cleaning

--- build_windows.py
+++ build_windows.py
@@ -5,6 +5,13 @@ with open("build_settings.json") as f:
 
 os.makedirs(settings["build_dir"], exist_ok=True)
 
+with open(settings["manifest_path"]) as f:
+    cargo = f.read()
+
+# Read some settings from the toml
+lib_name = re.findall("name\s*=\s*\"(.+)\"\n", cargo)[0]
+lib_version = re.findall("version\s*=\s*\"(.+)\"\n", cargo)[0]
+
 for rec in settings["archs"]:
     for name, target in rec["targets"].items():
         env = {
@@ -18,8 +25,12 @@ for rec in settings["archs"]:
             target.get("RUSTFLAGS", ""),
         ])
 
-        subprocess.check_call(
-            f"{settings['build_command']} --target {rec['triple']}",
-            env=env,
-            shell=True,
-        )
+        folder_name = f"{lib_name}_{rec['triple']}_{name}"        
+        
+
+        
+        #subprocess.check_call(
+        #    f"{settings['build_command']} --target {rec['triple']} --manifest-path={rec['manifest_path']}",
+        #    env=env,
+        #    shell=True,
+        #)
--- perf/README.md
+++
@@ -1,2 +0,0 @@
-# Perf
-This is an helper crate that we use to profile and optimize functions of the library.
